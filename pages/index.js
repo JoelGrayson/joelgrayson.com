a0a13942b071e862ccf35cc117cd24dfd58ae4c3
JoelGrayson <joel@joelgrayson.com>
2022-03-10 08:32:08 -0500

    Btn-red with home page

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,24 @@ import Image from 'next/image'
 import Page from './components/Page';
 import joelGraysonPortrait from './Home/Joel Grayson Portrait.webp';
 import signature from './home/signature.gif';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
     return (<Page>
+        <style jsx>{
+            `
+            .buttons { /* button grid */
+                display: -ms-grid;
+                display: grid;
+                -ms-grid-columns: 1fr 1fr 1fr;
+                    grid-template-columns: 1fr 1fr 1fr;
+            }
+            
+            .buttons li {
+                list-style-type: none;
+                margin: 0 auto;
+            }
+            `
+        }</style>
         <div className='w-full h-[300px] bg-white flex justify-center items-end'
             style={{background: "linear-gradient(120deg, rgba(255,255,255,1) 0%, rgba(216,216,216,1) 100%)"}}>
                 <h1 className='text-center pr-[118px] pb-[48px] relative'>
@@ -23,14 +37,14 @@ export default function Home() {
                 humanity will not go extinct. To understand more cultures, I am fluent in English and Chinese and am working on
                 Spanish. I enjoy chess, perform <a href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A'>comedy with Dad</a>, and play the violin.</p>
                 <p>Click below to explore some of my projects:</p>
-                <div className={styles.buttons}>
-                    <li><a href='/art'><button>Art</button></a></li>
-                    <li><a href='/machines'><button>Machines</button></a></li>
-                    <li><a href='/performances'><button>Performances</button></a></li>
-                    <li><a href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A'><button>Comedy with Dad</button></a></li>
-                    <li><button>Student Council Campaigns</button></li>
-                    <li><button>My Flag</button></li>
-                    <li><a href='https://lirongart.com'><button>Lirong Art (Business)</button></a></li>
+                <div className='buttons'>
+                    <li><a href='/art'><button className='btn-red'>Art</button></a></li>
+                    <li><a href='/machines'><button className='btn-red'>Machines</button></a></li>
+                    <li><a href='/performances'><button className='btn-red'>Performances</button></a></li>
+                    <li><a href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A'><button className='btn-red'>Comedy with Dad</button></a></li>
+                    <li><button className='btn-red'>Student Council Campaigns</button></li>
+                    <li><button className='btn-red'>My Flag</button></li>
+                    <li><a href='https://lirongart.com'><button className='btn-red'>Lirong Art (Business)</button></a></li>
                 </div>
             </div>
     </Page>);
