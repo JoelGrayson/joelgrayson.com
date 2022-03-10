2f27a84911dbc0ee644a1911997845f93553b113
JoelGrayson <joel@joelgrayson.com>
2022-03-10 08:32:14 -0500

    404 page with shrug image

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import Page from './components/Page';
+import shrug from './home/shrug.png';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Custom404() {
+    return (<Page>
+        <div className='flex justify-center items-center p-24'>
+            <div className="left">
+                <Image width='455px' height='342px' src={shrug} />
+            </div>
+            <div className="right flex flex-col items-center gap-8">
+                <h1 className='text-5xl font-bold'>404</h1>
+                <p>This page does not exist.</p>
+                <Link href='/'>
+                    <a>
+                        <button className='btn-red'>Return home</button>
+                    </a>
+                </Link>
+            </div>
+        </div>
+    </Page>);
+}
