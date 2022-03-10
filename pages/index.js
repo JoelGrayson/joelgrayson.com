847e4c222069b2bc5e80f111ca5c62f6020f3b87
JoelGrayson <joel@joelgrayson.com>
2022-03-10 10:17:21 -0500

    Nice top with signature & portrait

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,25 @@ export default function Home() {
             }
             `
         }</style>
-        <div className='w-full h-[300px] bg-white flex justify-center items-end'
-            style={{background: "linear-gradient(120deg, rgba(255,255,255,1) 0%, rgba(216,216,216,1) 100%)"}}>
-                <h1 className='text-center pr-[118px] pb-[48px] relative'>
-                    <Image src={signature} alt='Joel Grayson' width='371px' height='150px' /> {/* signature gif */}
-                </h1>
-                <Image src={joelGraysonPortrait} alt="Profile" id='profilePicture' height='270px' width='219px' /> {/* profile photo */}
+        <div className='w-full p-0 m-0'>
+            {/* top */}
+            <div className='w-full h-[290px] p-0'
+                style={{background: "linear-gradient(120deg, rgba(255,255,255,1) 0%, rgba(216,216,216,1) 100%)"}}
+            >
+                <div className='max-w-[800px] relative mx-auto h-full'> {/* container for images same width as content */}
+                    {/* signature gif */}
+                    <div className='absolute bottom-8 left-11'>
+                        <Image src={signature} alt='Joel Grayson' width='371px' height='150px' />
+                    </div>
+
+                    {/* portrait */}
+                    <div className='absolute p-0 bottom-0 mb-[-6px] right-24' style={{clipPath: 'inset(0 0 6px 1px)'}}>
+                        <Image src={joelGraysonPortrait} alt="Profile" id='profilePicture' height='270px' width='219px' />
+                    </div>
+                </div>
             </div>
-            <div id="main" className='j_container'>
+
+            <article className='j_container'>
                 <p>Hi friend! Thanks for stopping by. On this page, I will introduce myself. My intention is to share my projects.</p>
                 <p>I love to create. My three main interests are engineering, entrepreneurship, and politics.</p>
                 <p>I am passionate about engineering, Science, politics, and space. Since my Mom paints, and I like business, I
@@ -47,6 +58,7 @@ export default function Home() {
                     <li><button className='btn-red'>My Flag</button></li>
                     <li><a href='https://lirongart.com'><button className='btn-red'>Lirong Art (Business)</button></a></li>
                 </div>
-            </div>
+            </article>
+        </div>
     </Page>);
 }
