bce771be65b4ecaa24fb8a9b9b0c23e7277b3b95
JoelGrayson <joel@joelgrayson.com>
2022-03-22 11:36:33 -0400

    📣 Docker container! Yay! Wohoo!
    This Next.js application has been containerized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
     content: [ //all pages for compilation
-        './pages/*.{js,ts,jsx,tsx}',
         './pages/**/*.{js,ts,jsx,tsx}',
-        './components/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {}
     },
-    plugins: [require('tailwindcss')],
+    plugins: [],
 };
