738d577c86f4a7cf32e3e854da40628a2188804e
Joel Grayson <joel@joelgrayson.com>
2022-11-26 18:27:37 -0500

    Allow CORS for jg.com

--- api.joelgrayson.com/app.js
+++ api.joelgrayson.com/app.js
@@ -3,6 +3,19 @@ const app=express();
 const port=8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const cors=require('cors');
+const whitelist=[
+    'http://localhost:8080', 'https://localhost:3000',
+    'https://joelgrayson.com', 'https://www.joelgrayson.com'
+];
+app.use(cors({
+    origin: (origin, callback)=>{
+        if (whitelist.includes(origin))
+            callback(null, true);
+        else
+            callback(new Error('Not allowed by CORS'));
+    }
+}))
 
 app.get('/', (req, res)=>{
     res.send('Used for API requests that cannot be done on Next.js and require using Express.js');
