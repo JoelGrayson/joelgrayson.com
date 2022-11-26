12e54d607249c1a35c8ab3ee460eef1111c183dd
Joel Grayson <joel@joelgrayson.com>
2022-11-26 16:52:41 -0500

    Bug fix

--- pages/projects/performances/index.tsx
+++ pages/projects/performances/index.tsx
@@ -1,4 +1,5 @@
 import Page from '../../../components/Page';
+import Link from 'next/link';
 
 export async function getServerSideProps() {
     const url=`https://www.googleapis.com/youtube/v3/playlistItems?key=${process.env.GCLOUD_API_KEY}&part=snippet&playlistId=PLPq06AMW3cIFOA3J8Z0cd1bMaKn6TV_9K`;
@@ -18,8 +19,12 @@ export default function Performances({videos}: any) {
     return (<Page padding>
         <h1 className='text-center'>Performances</h1>
         {/* <p>Coming soon!</p> */}
-        https://www.youtube.com/playlist?list=PLPq06AMW3cIFOA3J8Z0cd1bMaKn6TV_9K
-        <ul>
+        {/* https://www.youtube.com/playlist?list=PLPq06AMW3cIFOA3J8Z0cd1bMaKn6TV_9K */}
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            gap: 10
+        }}>
             {videos.items.map((video: any)=>{
                 const ytUrl=`https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`;
                 let thumbnailUrl=video.snippet.thumbnails.high.url;
@@ -27,9 +32,15 @@ export default function Performances({videos}: any) {
                 //     thumbnailUrl=video.snippet.thumbnails?.maxres?.url;
 
                 console.log(thumbnailUrl);
-                return <a href={ytUrl}><img src={thumbnailUrl}></img></a>;
+                return (
+                    <div key={video.id}>
+                        <a><Link href={ytUrl}>
+                            <img src={thumbnailUrl}/>
+                        </Link></a>
+                    </div>
+                )
                 // return <li key={video.id}>{JSON.stringify(video)}</li>
             })}
-        </ul>
+        </div>
     </Page>);
 }
