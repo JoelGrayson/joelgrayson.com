09701cd0f44d7d2245989ee1d6fbd09e68b1ac85
Joel Grayson <joel@joelgrayson.com>
2023-02-24 11:03:28 -0500

    Sort by category

--- pages/perspective/index.tsx
+++ pages/perspective/index.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 import PerspectiveBody from '@/components/perspective/PerspectiveBody';
-import { articles } from '@/components/perspective/articles-list';
+import { article as articleT, articles, categories, displayCategory } from '@/components/perspective/articles-list';
 import Image from 'next/image';
 import Link from 'next/link';
+
+const gridStyle={
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))'
+};
 
 export default function JoelsPerspective() { //List of articles
     type sortingMethodT='newest to oldest' | 'oldest to newest' | 'by category' | 'A-Z' | 'Z-A';
@@ -17,6 +22,8 @@ export default function JoelsPerspective() { //List of articles
         }
     })();
     
+    console.log(sortedArticles);
+
     return <PerspectiveBody>
         <div className='flex justify-end mb-3'>
             <span>Sort by: </span>
@@ -28,31 +35,48 @@ export default function JoelsPerspective() { //List of articles
                 <option value='Z-A'>Z-A</option>
             </select>
         </div>
-        <div style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))'
-        }}>
-            { sortedArticles.map(article=>(
-                <Link key={article.hyphenatedName} href={`/perspective/${article.hyphenatedName}`}>
-                    {/* Thumbnail Heights
-                        CSS Size
-                            image height: 113px
-                            image width: 200px
-
-                        Actual Image Size (2x CSS Size)
-                            image height: 225px
-                            image width: 400px
-                    */}
-                    <div className='p-3 border-gray-400 border-[1px] m-2 rounded-xl shadow-md bg-gray-50 hover:shadow-slate-300 hover:shadow-lg cursor-pointer grid place-items-center' style={{
-                        height: '220px',
-                        width: '250px',
-                    }}>
-                        {/* <div>{JSON.stringify(article)}</div> */}
-                        <Image src={`/image/perspective/${article.hyphenatedName}/thumbnail.jpg`} alt={'Article thumbnail'} width='200' height='113' />
-                        <div className='text-center'>{article.name}</div>
-                    </div>
-                </Link>
-            )) }
-        </div>
+        {
+            sortingMethod==='by category'
+            ? <>
+                {categories.map(category=>{ 
+                    const { name, color }=displayCategory.get(category)!;
+                    return <div key={category} style={{ backgroundColor: color, borderRadius: 10 }} className='border-b-2 mb-1 shadow-sm px-3 py-3 pb-5 mb-4'>
+                        <h3 className='text-center bold'>{name}</h3> {/* Category name */}
+                        <div style={gridStyle}> {/* Articles item */}
+                            {articles.filter(a=>a.category===category).map(article=>( //get articles of a category
+                                <Article key={article.hyphenatedName} article={article} />
+                            ))}
+                        </div>
+                    </div>;
+                })}
+            </>
+            : <div style={gridStyle}>{
+                sortedArticles.map(article=>(
+                    <Article key={article.hyphenatedName} article={article} />
+                ))
+            }</div>
+        }
     </PerspectiveBody>;
+}
+
+export function Article({article}: {article: articleT}) { //rectangle with thumbnail and title
+    return <Link href={`/perspective/${article.hyphenatedName}`} style={{width: 'fit-content', margin: '0.5rem'}}>
+        {/* Thumbnail Heights
+            CSS Size
+                image height: 113px
+                image width: 200px
+
+            Actual Image Size (2x CSS Size)
+                image height: 225px
+                image width: 400px
+        */}
+        <div className='p-3 border-gray-400 border-[1px] rounded-xl shadow-md bg-gray-50 hover:shadow-slate-300 hover:shadow-lg cursor-pointer grid place-items-center' style={{
+            height: '220px',
+            width: '250px',
+        }}>
+            {/* <div>{JSON.stringify(article)}</div> */}
+            <Image src={`/image/perspective/${article.hyphenatedName}/thumbnail.jpg`} alt={'Article thumbnail'} width='200' height='113' />
+            <div className='text-center'>{article.name}</div>
+        </div>
+    </Link>
 }
