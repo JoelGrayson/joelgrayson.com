232a0d89926ce0e5eca764e059d2a4fed3e9cae0
Joel Grayson <joel@joelgrayson.com>
2023-02-27 11:20:16 -0500

    ASCII console.log of j with three dots on all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,28 @@ import { QueryClientProvider, QueryClient } from 'react-query';
 
 function MyApp({ Component, pageProps }: AppProps) {
     const queryClient=new QueryClient();
+    
+    console.log(
+`┏---------------------------------------┓
+|                                       |
+|            jjjjjjjjj                  |
+|        jjjjjjjjjjjjjjj                |
+|     jjjjjjjjjjjjjjjjjjj               |
+|    jjjjjjjjj      jjjjj   jjjjjjjjj   |
+|    jjjjj          jjjjjjjjjjjjjjjjj   |
+|                  jjjjjjjjjjjjj        |
+|              jjjjjjjjjj               |
+|           jjjjjjjjjjjj  jjjj    jjjj  |
+|        jjjjjjjjjjjjjj  jjjjjj  jjjjjj |
+|      jjjjjjjjjjjjjjjj   jjjj    jjjj  |
+|    jjjjjjjj   jjjjjj        jjjj      |
+|   jjjjjj    jjjjjjj        jjjjjj     |
+|  jjjjj    jjjjjjjj          jjjj      |
+|  jjjjj  jjjjjjjj                      |
+|  jjjjjjjjjjjj                         |
+|   jjjjjjjj           joelgrayson.com  |
+|                                       |
+┖---------------------------------------┛`);
 
     return (
         <QueryClientProvider client={queryClient}>
