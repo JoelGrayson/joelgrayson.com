acbe6b9be1a2ed5c61c5981f4ec030474b73369c
Joel Grayson <joel@joelgrayson.com>
2023-02-28 11:20:21 -0500

    Slaphappy page

--- components/Page.tsx
+++ components/Page.tsx
@@ -20,12 +20,13 @@ export default function Page({children, title, padding, noheader=false, ...props
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
 
-        <main style={{
+        <main {...props} style={{
             position: 'relative',
             padding: 0, margin: 0,
             width: '100%',
-            minHeight: 'calc(100vh - 77px - 89px)' // fills the entire page (minus footer and header's heights)
-        }} {...{props}}>
+            minHeight: 'calc(100vh - 77px - 89px)', // fills the entire page (minus footer and header's heights)
+            ...props.style
+        }}>
             { padding
             ? <div className='j_container j_max-w'>{children}</div> //if center, wrap in centering container
             : children }
--- pages/projects/slaphappy/index.tsx
+++ pages/projects/slaphappy/index.tsx
@@ -1,12 +1,70 @@
 import Page from '@/components/Page';
 import Link from 'next/link';
+import Image from 'next/image';
+import { useRef, useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
 
 export default function Slaphappy() {
     // https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A
     
-    return <Page padding title='Art'>
-        <h1 className='text-center'>Slaphappy</h1>
+    const curtainsRef=useRef();
+
+    useLayoutEffect(()=>{
+        const el=curtainsRef.current!;
+        gsap.fromTo(
+            el,
+            { scale: 3 },
+            { scale: 1 }
+        );
+    }, []);
+    
+    return <Page title='Art' style={{overflow: 'hidden'}}>
+        {/* Curtain background image */}
+        <img src="/image/slaphappy/curtains.png" ref={curtainsRef} alt="Curtains" style={{
+            position: 'absolute',
+            top: -30,
+            left: 0,
+            width: '100vw',
+            zIndex: -1
+        }} />
         
-        <Link href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A'>Go to YouTube Channel</Link>
+        <img src='/image/slaphappy/Channel-Art.png' alt='Slaphappy Channel Art' className='mx-auto pt-[60px] sm:pt-[100px] lg:pt-[160px]' style={{
+            width: '60%',
+            zIndex: -2,
+            position: 'inherit'
+        }} />
+
+        <div className='items-center md:flex md:gap-8'>
+            <div style={{
+                backgroundColor: '#ffde5a',
+                border: '4px solid #ff5758',
+                borderRadius: 10,
+                width: 'fit-content',
+                marginLeft: '20%',
+                marginTop: 50,
+                padding: '9px 25px',
+            }}>
+                <div className='text-xl bold pb-0'>Hilarity Guaranteed!</div>
+                <div className='text-sm font-semibold pt-2 text-center'>*or your money back</div>
+            </div>
+
+            <div className='pl-[20%] md:pl-0' style={{
+                height: 'fit-content',
+                marginTop: 20,
+                display: 'flex',
+                gap: 10
+            }}>
+                <Link href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A?sub_confirmation=1'>
+                    <Image src='/image/slaphappy/Subscribe.png' alt='Subscribe' width='82' height='22' />
+                </Link>
+                <span>← Why not?</span>
+            </div>
+        </div>
+
+        <div style={{width: '60%', margin: '0 auto'}}>
+            <p>Slaphappy shows videos my Dad and I have made together since I was 11. If you find them funny, our names are Joel Grayson and Paul Grayson. If not, they are anonymous. The channel has separate sections for stop motion animation and my various projects. I am not a professional trumpet player.</p>
+        </div>
+        
+        <br /><br />
     </Page>;
 }
