cc05858ffbf65e0f8e11be21d6802bfdf3081bd4
Joel Grayson <joel@joelgrayson.com>
2023-05-15 19:12:58 -0400

    Updated perspective articles

---
+++ pages/perspective/what-it-means-to-be-an-american.tsx
@@ -0,0 +1,10 @@
+import ArticleWrapper from '@/components/perspective/ArticleWrapper';
+import { P } from '@jcomponents/writing-components';
+
+export default function WhatItMeansToBeAnAmerican() {
+    return <ArticleWrapper title='What It Means to Be an American' date={new Date('July 4, 2022')} hyphenatedTitle='what-it-means-to-be-an-american'>
+        <P>The United States is a safe haven and place of opportunity for my family. The United States allowed my father’s side to escape the harsh treatment of Jews in Eastern Europe and provided my mother with a new future to escape her harsh Chinese upbringing. On my mother's side, I am the first of four generations to be unaffected by the Cultural Revolution. It is incredible that one generation (my Mom in China) can have a horrible upbringing but the next generation (me in the United States) can live comfortably and freely.</P>
+        <P>It is true the United States has not always lived up to its ideals. However, today on American Independence Day, it is meaningful to remember the people before me who have brought me to this point where I can enjoy my life. It means remembering the values of freedom and democracy, which have allowed me to think independently and voice my opinion. It means stopping to acknowledge what we often take for granted in our everyday lives.</P>
+        <P>The greatness of the United States comes from our beliefs in the rights of the people and our value of unity: allowing immigrants from around the world to come together under a shared identity. We need to strengthen these two values that are waning in the current political environment to restore the greatness of America.</P>
+    </ArticleWrapper>;
+}
