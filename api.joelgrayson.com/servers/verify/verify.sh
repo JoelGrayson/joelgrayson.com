e5223a0d25d62942a057a0b2e2e4c38551a0507e
Joel Grayson <joel@joelgrayson.com>
2023-06-14 08:18:55 -0400

    removing signatures

--- api.joelgrayson.com/servers/verify/verify.sh
+++ api.joelgrayson.com/servers/verify/verify.sh
@@ -3,5 +3,8 @@
 # Input: Needs `signature.txt`, `message.txt`, and `public.pem`
 # Output: in console
 
-openssl enc -d -a -in './signature.txt' -out './signature.bin' #turn txt signature to hex
-openssl sha1 -verify public.pem -signature './signature.bin' './message.txt'
+sig_file='../../private/signature'
+
+openssl enc -d -a -in "$sig_file.txt" -out "$sig_file.bin" #turn txt signature to hex
+openssl sha1 -verify public.pem -signature "$sig_file.bin" './message.txt'
+
