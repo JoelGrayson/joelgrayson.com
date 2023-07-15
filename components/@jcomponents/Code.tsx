0509cef178de8e358d9013c8216919edf5ef6730
Joel Grayson <joel@joelgrayson.com>
2023-07-15 16:02:13 -0400

    bug fix

--- components/@jcomponents/Code.tsx
+++ components/@jcomponents/Code.tsx
@@ -1,12 +1,17 @@
-import { CopyBlock, dracula } from 'react-code-blocks';
+import { CopyBlock, github } from 'react-code-blocks';
 
 export default function Code({children, language='jsx'}: {children: string; language?: string}) {
     return <div style={{fontFamily: 'fira code'}}>
-            <CopyBlock
-                language={language}
-                text={children.trim()}
-                theme={dracula}
-                codeBlock
-            />
+        <CopyBlock
+            language={language}
+            text={children.trim()}
+            showLineNumbers={false}
+            theme={{...github, mode: 'dark' }}
+            wrapLongLines={false}
+            copied={false}
+            onCopy={()=>{}}
+            codeBlock
+        />
     </div>;
 }
+
