5bb01d16c4873c4a6730c48fa6889da27bc1422b
Joel Grayson <joel@joelgrayson.com>
2023-08-11 08:26:09 -0400

    joelgrayson.com/research/organic-optoelectronics

---
+++ pages/research/index.tsx
@@ -0,0 +1,14 @@
+import Page from '@/components/global/Page';
+import Button from '@jcomponents/button';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Research() {
+    return <Page bottomPadding>
+        <h1 className='text-center'>Research</h1>
+
+        <Link href='/research/organic-optoelectronics' className='styled'>
+            Twisting Crystal Charge Transfer Complexes for Organic Optoelectronics
+        </Link>
+    </Page>;
+}
