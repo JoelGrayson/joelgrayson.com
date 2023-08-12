362d13c088f362d3c92efba990d762665ef61d45
Joel Grayson <joel@joelgrayson.com>
2023-08-12 11:15:36 -0400

    /research includes term paper

--- pages/research/index.tsx
+++ pages/research/index.tsx
@@ -7,8 +7,11 @@ export default function Research() {
     return <Page bottomPadding>
         <h1 className='text-center'>Research</h1>
 
-        <Link href='/research/organic-optoelectronics' className='styled'>
+        <p><Link href='/research/organic-optoelectronics' className='styled'>
             Twisting Crystal Charge Transfer Complexes for Organic Optoelectronics
-        </Link>
+        </Link></p>
+        <p><Link href='/perspective/chinas-brutal-road-to-socialism' className='styled'>
+            China's Brutal Road to Socialism
+        </Link></p>
     </Page>;
 }
