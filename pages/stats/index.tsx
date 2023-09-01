76ef534cf2371d321b6491bd9afe98c402315007
Joel Grayson <joel@joelgrayson.com>
2023-09-01 11:38:59 -0400

    joelgrayson.com/stats

---
+++ pages/stats/index.tsx
@@ -0,0 +1,22 @@
+import Page from '@/components/global/Page';
+
+export default function Stats() {
+    return <Page>
+        <h1>Stats</h1>
+        <div id='table' className='grid grid-cols-2'>
+            <p>Focus installations</p>
+            <p></p>
+            <p>Homework checker installations</p>
+            <p></p>
+            <p>Perspective views</p>
+            <p></p>
+            <p>Buseroo.com users</p>
+            <p></p>
+            <p>ShanghaiDictionary.com searches</p>
+            <p></p>
+            <p>YouTube subscribers</p>
+            <p></p>
+        </div>
+    </Page>;
+}
+
