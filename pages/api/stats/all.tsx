10c77382f094b9a77cb906729946bdea0cf2c729
Joel Grayson <joel@joelgrayson.com>
2023-09-23 12:32:24 -0400

    /api/stats/all

---
+++ pages/api/stats/all.tsx
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from 'next/types';
+
+export default async function handler(globalReq: NextApiRequest, globalRes: NextApiResponse<any>) {
+    const apiKey=process.env.NODE_ENV==='development' ? process.env.NEXT_PUBLIC_DEVELOPMENT_YOUTUBE_API_KEY : process.env.NEXT_PUBLIC_PRODUCTION_YOUTUBE_API_KEY;
+    const slaphappyId='UCAwfG8BfhLuhMddFZh7z09A';
+    const joelgrayson2Id='UChs2-tks6XqrAdZ6K2I0QCA';
+    const channelId=slaphappyId+','+joelgrayson2Id;
+
+    const fetchRequests=[
+        fetch('https://api.joelgrayson.com/homepage-stats')
+            .then(res=>res.json())
+            .then((res)=>{
+                return {
+                    homeworkCheckerInstalls: res.hCInstalls,
+                    focusInstalls: res.focusInstalls
+                };
+            }),
+        fetch('https://joelgrayson.com/api/perspective/total-views')
+            .then(res=>res.json())
+            .then(res=>({
+                perspectiveViews: res.views
+            })),
+        fetch('https://shanghaidictionary.com/api/analytics/searches')
+            .then(res=>res.json())
+            .then(res=>({
+                shanghaiDictionarySearches: res.searches
+            })),
+        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`)
+            .then(res=>res.json())
+            .then(res=>{
+                const slaphappy=res.items.find((channel: any)=>channel.id===slaphappyId);
+                const joelgrayson2=res.items.find((channel: any)=>channel.id===joelgrayson2Id);
+                return {
+                    slaphappy: {
+                        subscribers: parseInt(slaphappy.statistics.subscriberCount),
+                        views: parseInt(slaphappy.statistics.viewCount)
+                    },
+                    joelgrayson2: {
+                        subscribers: parseInt(joelgrayson2.statistics.subscriberCount),
+                        views: parseInt(joelgrayson2.statistics.viewCount)
+                    }
+                };
+            })
+    ];
+
+    const stats=(await Promise.all(fetchRequests))
+        .reduce((acc, cur)=>({...acc, ...cur}), {});
+
+    globalRes.json(stats);
+}
