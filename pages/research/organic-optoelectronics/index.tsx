b05f70ba44f7ef684fd2e70d4a4812f6e3cd40a8
Joel Grayson <joel@joelgrayson.com>
2023-10-01 09:51:27 -0400

    improved research/organic-optoelectronics

--- pages/research/organic-optoelectronics/index.tsx
+++ pages/research/organic-optoelectronics/index.tsx
@@ -5,11 +5,16 @@ import { Data } from './data';
 
 export default function OrganicOptoelectronics() {
     return <Page bottomPadding>
-        <h1 className='text-center'>Twisting Crystals</h1>
+        <style jsx>{`
+            h3 {
+                margin-top: 1.5rem;
+            }
+        `}</style>
+        <h1 className='text-center'>Organic Optoelectronics</h1>
 
-        <p>Studying the charge transfer complex between the CTC of bis(4-bromophenyl)amine and 4,4&quot;-azopyridine (abbreviated as BrDPA-AzoBipy). It looks like this:</p>
+        <p>This research studied the charge transfer complex of bis(4-bromophenyl)amine and 4,4&quot;-azopyridine (abbreviated as BrDPA-AzoBipy), shown below:</p>
         <Image src='/research/organic-optoelectronics/BrDPA-AzoBipy Structure.png' width={150} height={230} alt='BrDPA-AzoBipy structure' />
-        <br />
+        <p>The goal was to find optimal twisting conditions so it can be used in organic optoelectronics&apos;s active layer.</p>
         
         <h3>Optimal Conditions for Twisting BrDPA-AzoBipy</h3>
         <ul>
@@ -17,8 +22,7 @@ export default function OrganicOptoelectronics() {
             <li>Melting temperature: 140°C</li>
             <li>Cooling temperature: 70°C</li>
         </ul>
-        <br />
-        
+                
         <h3>Characterization</h3>
         <ul>
             <li>Absorbance of a wide spectrum.</li>
@@ -26,11 +30,9 @@ export default function OrganicOptoelectronics() {
             <li><a className='styled' href="/research/organic-optoelectronics/X-ray-diffraction.zip">X-Ray diffraction (from UNAM)</a></li>
         </ul>
         {/* <p>TODO: test it in more devices like photodetectors and, if successful, organic solar cells.</p> */}
-        <br />
-
-        <Data />
-        <br />
         
+        <Data />
+                
         <h3>Weekly presentations</h3>
         <PDF src='/research/organic-optoelectronics/presentations/Week 1.pdf' height={464} />
         <PDF src='/research/organic-optoelectronics/presentations/Week 2.pdf' height={464} />
@@ -39,17 +41,17 @@ export default function OrganicOptoelectronics() {
         <PDF src='/research/organic-optoelectronics/presentations/Week 5.pdf' height={464} />
         
         <br />        
-        <h3>Learn More</h3>
-        <p>Explaining stuff in the lab to a general audience</p>
+        <h3>Explaining stuff in the lab to a general audience</h3>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/AuM790sAzUc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
-        <p>Presenting at the ARISE colloquium</p>
+        
+        <h3>Presenting at the ARISE colloquium</h3>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/kJgvV70m3Jg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+        <p>This research was featured on <a href="https://ny1.com/nyc/all-boroughs/news/2023/08/17/students-work-on-real-world-problems-at-nyu-summer-stem-program" className='styled'>Spectrum NY1&apos; local news</a>.</p>
     
-        <p>Poster</p>
+        <h3>Poster</h3>
         <PDF src='/research/organic-optoelectronics/Poster.pdf' height={464} />
         
-        <p>This research was mentioned on <a href="https://ny1.com/nyc/all-boroughs/news/2023/08/17/students-work-on-real-world-problems-at-nyu-summer-stem-program" className='styled'>Spectrum NY1 at ny1.com/nyc/all-boroughs/news/2023/08/17/students-work-on-real-world-problems-at-nyu-summer-stem-program</a>.</p>
-        <p>Research paper (coming soon)</p>
+        <h3>Research Paper</h3>
         <PDF src='/research/organic-optoelectronics/Twisting Charge Transfer Complex Crystals for Organic Optoelectronics Paper.pdf' height={700} />
     </Page>;
 }
