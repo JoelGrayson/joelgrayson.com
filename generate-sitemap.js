6636b19dd67cefb5af16546b6f154780456bdd26
Joel Grayson <joel@joelgrayson.com>
2023-10-04 19:16:54 -0400

    generate sitemap

---
+++ generate-sitemap.js
@@ -0,0 +1,27 @@
+const { glob }=require('glob');
+const fs=require('fs');
+
+async function getXML() {
+    const pages=(await glob('pages/**/*.{ts,tsx,js,jsx}', { ignore: ['pages/_*.tsx', 'pages/api/**/*'], cwd: __dirname }))
+        .map(str=>str.match(/^pages\/(.*)\.(?:ts|tsx|js|jsx)$/)[1])
+        .filter(str=>str.slice(3)!=='api')
+        .map(str=>{
+            if (str.slice(-6)==='/index')
+                return str.slice(0, -6);
+            else
+                return str;
+        });
+    const xml=`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages.map(str=>`    <url>
+        <loc>https://joelgrayson.com/${str}</loc>
+    </url>`).join('\n')}
+</urlset>`;
+    return xml;
+}
+
+async function main() {
+    fs.writeFile('public/sitemap.xml', await getXML(), ()=>{});
+}
+
+main();
--- pages/patriotism/index.tsx
+++ pages/patriotism/index.tsx
@@ -1,12 +1,9 @@
 import Page from '@/components/global/Page';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import NextImage from 'next/image';
 import { PlayAudioIcon } from '@/components/Icons';
 import { Tooltip } from 'antd';
-
-const size=141;
-const poleWidth=5;
-const poleLeftOffset=3;
+import Flag from '@jcomponents/flag';
 
 export default function Patriotism() {
     const [selectedFlag, setSelectedFlag]=useState<null | 'usa' | 'nyc' | 'ucjg'>(null);
@@ -85,70 +82,5 @@ export default function Patriotism() {
             </div> }
         </div>
     </Page>;
-}
-
-export function Flag({ src, selected, ...props }: { src: string; selected: boolean; [key: string]: any }) {
-    const canvasRef=useRef<HTMLCanvasElement>(null);
-
-    useEffect(()=>{
-        const canvas=canvasRef.current;
-        const c=canvas?.getContext('2d');
-        if (!c) return;
-
-        // Adapted from http://bucarotechelp.com/design/jseasy/81071103.asp
-        const pattern=new Image();
-        pattern.src=src;
-        
-        var ph=pattern.height;
-        
-        const intervalId=setInterval(()=>{
-            var iw=pattern.width;
-            var ih=pattern.height;
-            c.clearRect(0, 0, size*2, size*2);
-
-            if (ph<=0)
-                ph=ih;
-            else
-                ph-=20;
-
-            const waveHeight=4, frequency=18, verticalOffset=10;
-
-            c.fillStyle='#303030';
-            c.strokeStyle='#303030';
-            c.rect(poleLeftOffset, verticalOffset-5, poleWidth, size*.85);
-            c.fill();
-            // circle
-            c.strokeStyle='#dbaf42';
-            c.fillStyle='#dbaf42';
-            c.beginPath();
-            c.arc(poleLeftOffset+poleWidth/2, verticalOffset-5, 4, 0, 2*Math.PI);
-            c.stroke();
-            c.fill();
-
-            for (var x=0; x<iw; x++) {
-                var y=waveHeight*Math.sin((x+ph)/frequency)+verticalOffset;
-                c.drawImage(pattern, x, 0, 1, ih, x+poleLeftOffset+poleWidth, y, 1, ih);
-            }
-        }, 80);
-
-        return ()=>clearInterval(intervalId);
-    }, [src]);
-    
-    return <canvas
-        style={{
-            padding: '30px 33px',
-            borderTopLeftRadius: '15px',
-            borderTopRightRadius: '15px',
-            paddingBottom: '0',
-            borderTop: selected ? '1px solid black' : '1px solid transparent',
-            borderRight: selected ? '1px solid black' : '1px solid transparent',
-            borderLeft: selected ? '1px solid black' : '1px solid transparent',
-            borderBottom: 'none',
-            backgroundColor: selected ? 'white' : undefined,
-            zIndex: 1
-        }}
-        width={size+2*poleWidth+poleLeftOffset} height={size*.85}
-        ref={canvasRef} {...props}
-    />;
 }
 
