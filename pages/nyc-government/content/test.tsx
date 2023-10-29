96d48477990277fbe70f5e0c89321c2c52b692e1
Joel Grayson <joel@joelgrayson.com>
2023-10-29 16:36:47 -0400

    more questions in nyc government

--- pages/nyc-government/content/test.tsx
+++ pages/nyc-government/content/test.tsx
@@ -23,6 +23,12 @@ export default function Test() {
             answer={1}
             number={questionNumber++}
         />
+        <Question
+            type='text'
+            question='Through what process do zoning map changes go through?'
+            answer={['ULURP', 'ulurp', 'Uniform Land Use Review Procedure']}
+            number={questionNumber++}
+        />
     </Page>;
 }
 
@@ -30,14 +36,21 @@ export default function Test() {
 export function Question({ type, question, answer, choices, extra=undefined, number }: {
     type: 'text' | 'reveal' | 'multiple choice';
     question: JSX.Element | string;
-    choices: Array<JSX.Element | string>;
-    answer: JSX.Element | string | number /* index of multiple choice*/;
+    choices?: Array<JSX.Element | string>;
+    answer: JSX.Element | string | number /* index of multiple choice*/ | string[] /* multiple valid answers */;
     extra?: JSX.Element | string | Array<JSX.Element | string>;
-    number?: number;
+    number: number;
 }) {
     const [answered, setAnswered]=useState<boolean>(false);
     const [userInput, setUserInput]=useState<number | string>('');
-
+    let correct=answer==userInput;
+    if (type==='text' && Array.isArray(answer))
+        correct=answer.includes(userInput as string);
+    if (type==='multiple choice') {
+        if (!choices)
+            throw new Error('Multiple choice question must have choices');
+    }
+    
     return <form className='relative' onSubmit={e=>{
         setAnswered(true);
         e.preventDefault();
@@ -54,9 +67,9 @@ export function Question({ type, question, answer, choices, extra=undefined, num
                     answered
                     ? <>
                         <div className='answer'>{answer}</div>
-                    { extra && <div className='extra'>{extra}</div> }
+                        { extra && <div className='extra'>{Array.isArray(extra) ? extra[0] : extra}</div> }
                     </>
-                    : <button type="submit">Submit</button>
+                    : <Button type="submit">Submit</Button>
                 }
             </>,
             'reveal': <>
@@ -66,11 +79,11 @@ export function Question({ type, question, answer, choices, extra=undefined, num
                         <div className='answer'>{answer}</div>
                     { extra && <div className='extra'>{extra}</div> }
                     </>
-                    : <button type="submit">Submit</button>
+                    : <Button type="submit">Submit</Button>
                 }
             </>,
             'multiple choice': <>
-                {choices.map((choice, i)=>{
+                {choices?.map((choice, i)=>{
                     const id=`question-${number}-choice-${i}`;
                     let className;
                     if (answered) {
@@ -94,18 +107,53 @@ export function Question({ type, question, answer, choices, extra=undefined, num
                     answered
                     ? <>
                         { extra && <div className='extra'>{extra}</div> }
-                        { answer==userInput ?
+                        { correct ?
                             // Checkmark
                             <svg className='absolute top-[-10px] left-[-40px] w-10 h-10' fill='green' version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg"> <path d="m958.96 406.92-65.746-68.016-419.8 406.95-162.1-180.24-70.281 63.102 194.59 216.89c5.793 6.2969 12.848 10.832 21.16 13.602 8.3125 2.5195 16.625 2.7734 24.938 0.75781 8.3164-2.2695 15.621-6.3008 21.918-12.094z"/> </svg>
                             :
                             // X mark
                             <svg className='absolute top-[0px] left-[-35px] w-6 h-6' fill='red' version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg"> <path d="m712.7 600 276.77-276.77c7.3242-7.3203 7.3242-19.195 0-26.516l-86.18-86.18c-7.3203-7.3242-19.195-7.3242-26.516 0l-276.77 276.77-276.77-276.77c-7.3242-7.3242-19.195-7.3242-26.516 0l-86.18 86.18c-7.3203 7.3203-7.3203 19.195 0 26.516l276.77 276.77-276.77 276.77c-7.3203 7.3242-7.3203 19.195 0 26.516l86.18 86.18c7.3242 7.3203 19.195 7.3203 26.516 0l276.77-276.77 276.77 276.77c7.3203 7.3203 19.195 7.3203 26.516 0l86.18-86.18c7.3242-7.3242 7.3242-19.195 0-26.516z"/> </svg>
-
                         }
                     </>
                     : <Button type="submit">Submit</Button>
                 }
-            </>
+            </>,
+            // 'multiple choice checkmarks': <>
+            //     {choices?.map((choice, i)=>{
+            //         const id=`question-${number}-choice-${i}`;
+            //         let className;
+            //         if (answered) {
+            //             if (answer===i) 
+            //                 className='text-green-900 bg-green-200';
+            //             else
+            //                 className='text-red-900 bg-red-200';
+            //         }
+            //         return <div key={i} className={className}>
+            //             <input type="checkbox"
+            //                 value={i}
+            //                 id={id}
+            //                 checked={userInput==i}
+            //                 onChange={e=>setUserInput(e.target.value)}
+            //                 disabled={answered}
+            //             />
+            //             <label htmlFor={id} className='ml-3'>{choice}</label>
+            //         </div>;
+            //     })}
+            //     {
+            //         answered
+            //         ? <>
+            //             { extra && <div className='extra'>{extra}</div> }
+            //             { correct ?
+            //                 // Checkmark
+            //                 <svg className='absolute top-[-10px] left-[-40px] w-10 h-10' fill='green' version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg"> <path d="m958.96 406.92-65.746-68.016-419.8 406.95-162.1-180.24-70.281 63.102 194.59 216.89c5.793 6.2969 12.848 10.832 21.16 13.602 8.3125 2.5195 16.625 2.7734 24.938 0.75781 8.3164-2.2695 15.621-6.3008 21.918-12.094z"/> </svg>
+            //                 :
+            //                 // X mark
+            //                 <svg className='absolute top-[0px] left-[-35px] w-6 h-6' fill='red' version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg"> <path d="m712.7 600 276.77-276.77c7.3242-7.3203 7.3242-19.195 0-26.516l-86.18-86.18c-7.3203-7.3242-19.195-7.3242-26.516 0l-276.77 276.77-276.77-276.77c-7.3242-7.3242-19.195-7.3242-26.516 0l-86.18 86.18c-7.3203 7.3203-7.3203 19.195 0 26.516l276.77 276.77-276.77 276.77c-7.3203 7.3242-7.3203 19.195 0 26.516l86.18 86.18c7.3242 7.3203 19.195 7.3203 26.516 0l276.77-276.77 276.77 276.77c7.3203 7.3203 19.195 7.3203 26.516 0l86.18-86.18c7.3242-7.3242 7.3242-19.195 0-26.516z"/> </svg>
+            //             }
+            //         </>
+            //         : <Button type="submit">Submit</Button>
+            //     }
+            // </>
         }[type]}
     </form>;
 }
