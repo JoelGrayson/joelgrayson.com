2a34b6dd29acf9550e73b339217eb3beb4acfdac
Joel Grayson <joel@joelgrayson.com>
2023-11-22 09:27:07 -0500

    joel's plan updated SEB agenda & style

--- pages/plan/index.tsx
+++ pages/plan/index.tsx
@@ -6,7 +6,7 @@ export const agendas={
     Connect2Grid: '1m6f8igALdkPsQkEG3wmODTCtMzHwqOytguZQ_VUzr08',
     CB1: '1CFfa8K-XeO4x9fvJ9qwHT2XYjqRlWax7zUnb1yoXRVc',
     Solar: '1WQnU_U1rG955NcYjRCCz7fBeK8wEZ2KxW7_JBN7mmS4',
-    'Students for Electric Buses': '1pRmHK0qcHAmzz2aK2XTQugy-FfjTtm1Ugc-fqv99Kxg',
+    'Students for Electric Buses': '1hjc1ZM9gfHd7VsPnQcnHtzrVOx8RguzEajhax7rk7Ww',
     'Vending Machine': '17fWXdeUmfWz0W7UqSDC0tmr5TaMrz6L7O2S9Uu6iS34',
     'Learning': '15TW1rDATUrihnRX3cuLSuL-Q6fZZR1JwRtd_uQHE9A4'
 };
@@ -20,13 +20,14 @@ export default function Plan() {
         </Head>
         <h1 className="text-center">Joel&apos;s Plan</h1>
 
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 gap-5 mx-5">
             {
                 // eslint-disable-next-line
                 Object.entries(agendas).map(([name, docId], i)=>{
                     return <iframe key={docId} src={docLink(docId)} style={{
-                        width: 750,
-                        height: 500
+                        width: '100%',
+                        height: 500,
+                        border: '1px solid gray'
                     }} />;
                 })
             }
@@ -37,7 +38,7 @@ export default function Plan() {
                 <ul className="mt-8 text-lg">
                     <li>Shirtocracy.com</li>
                     <li>BulletBrainstorm.com</li>
-                    <li>memorizethepresidents.com</li>
+                    <li>MemorizeThePresidents.com</li>
                 </ul>
             </div>
         </div>
