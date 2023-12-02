d33c6c4c2e5399e4d7481283a5ca241c6c6052a1
Joel Grayson <joel@joelgrayson.com>
2023-12-02 17:28:45 -0500

    able to build static html files

--- next.config.js
+++ next.config.js
@@ -6,18 +6,24 @@ module.exports={
     reactStrictMode: true,
     swcMinify: true,
     redirects: jredirects,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.ytimg.com',
-                pathname: '/vi/**',
-            }
-        ]
-    },
     i18n: {
         locales: ['en'],
         defaultLocale: 'en'
     },
+
+    // Uncomment below in order to export as HTML
+    // output: 'export',
+    // basePath: '/joelgrayson.com',
+    // assetPath: '/joelgrayson.com',
+    // images: {
+    //     unoptimized: true,
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'i.ytimg.com',
+    //             pathname: '/vi/**',
+    //         }
+    //     ]
+    // },
 };
 
