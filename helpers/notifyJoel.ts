04ab6af2a0a5c56a66fd4c63fb63cfe0e9df9a66
Joel Grayson <joel@joelgrayson.com>
2023-12-23 16:19:35 -0500

    not using texting because of $12 per month cost and the fact that anything done on the phone could probably be done better over email

--- helpers/notifyJoel.ts
+++ helpers/notifyJoel.ts
@@ -1,6 +1,5 @@
 const emailClient=require('@sendgrid/mail');
 emailClient.setApiKey(process.env.SENDGRID_API_KEY);
-// const textClient=require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export default function notifyJoel(args: { //notify Joel of contact form submission
     text?: string;
@@ -20,13 +19,4 @@ export default function notifyJoel(args: { //notify Joel of contact form submiss
             replyTo: args.email.replyTo
         });
     }
-
-    // text Joel (not using because of $12 per month cost and the fact that anything done on the phone could probably be done better over email)
-    // if (args.text) {
-    //     textClient.messages.create({
-    //         to: process.env.MY_PHONE_NUMBER,
-    //         from: process.env.TWILIO_PHONE_NUMBER,
-    //         body: args.text,
-    //     });
-    // }
 }
