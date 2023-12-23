7632dc6d828f130d4f3bff65a4a3464b927c5b96
Joel Grayson <joel@joelgrayson.com>
2023-12-23 14:35:17 -0500

    logs when and which URL users misspelled joelgrayson.com

---
+++ pages/api/log-error/joelgreyson.com/index.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/data/prisma/client';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+    const url=req.body.url;
+
+    await prisma.error.create({
+        data: {
+            date: new Date(),
+            source: url || 'unknown',
+            message: 'joelgreyson.com'
+        }
+    });
+
+    res.json({success: true});
+}
