fe7f0906ca4923ab1f2477590a799044f47a5eeb
Joel Grayson <joel@joelgrayson.com>
2024-01-12 07:51:48 -0500

    update CORS policy

--- next.config.js
+++ next.config.js
@@ -45,8 +45,8 @@ const nextConfig={
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, //the origin
-                    { key: "Access-Control-Allow-Methods", value: "POST" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    { key: "Access-Control-Allow-Methods", value: "POST, GET" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, X-Requested-With" },
                 ]
             }
         ]
