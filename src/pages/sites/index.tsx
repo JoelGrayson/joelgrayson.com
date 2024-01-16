bb0bb519293fe6b974a69fb78387561249410179
Joel Grayson <joel@joelgrayson.com>
2024-01-16 06:48:24 -0500

    /sites works

--- src/pages/sites/index.tsx
+++ src/pages/sites/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import Container from '@jcomponents/container';
 
 export default function SitesPage() {
@@ -12,24 +12,26 @@ export function Sites() { /* bullet points of all sites */
         <h1 className='text-center'>Sites</h1>
         <ul>
             <Site>joelgrayson.com</Site>
-            <Site>buseroo.com (startup)</Site>
-            <Site>lirongart.com (startup)</Site>
-            <Site href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A'>youtube.com/@JoelGrayson (Slaphappy)</Site>
+            <Site addendum=' (startup)'>buseroo.com</Site>
+            <Site addendum=' (startup)'>lirongart.com</Site>
+            <Site addendum=' (Slaphappy)' href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A'>youtube.com/@JoelGrayson</Site>
             <Site>studentsforelectricbuses.org</Site>
             <Site>luxpremierllc.com</Site>
             <Site>memorizethepresidents.com</Site>
-            <li><a target='_blank' href='https://joelgrayson.wixsite.com/geography'>Geography learning site</a> (my first site)</li>
+            <Site addendum=' (startup)'>shirtocracy.com</Site>
+            <Site addendum=' (my first site)' href='https://joelgrayson.wixsite.com/geography'>Geography learning site</Site>
+        </ul>
 
-            <br />
-            <h3>Coming Soon</h3>
-            <Site>bulletbrainstorm.com (startup)</Site>
-            <Site>shirtocracy.com (startup)</Site>
+        <h3 className='mt-3'>Coming Soon</h3>
+        <ul>
+            <Site addendum=' (startup)'>bulletbrainstorm.com</Site>
         </ul>
     </>;
 }
 
-export function Site({children, href}: {children: string, href?: string}) {
+export function Site({ children, href, addendum }: { children: string; href?: string; addendum?: string | ReactNode }) {
     return <li>
         <a href={href || `https://${children}`} target='_blank'>{children}</a>
+        { addendum && <span>{addendum}</span> }
     </li>;
 }
