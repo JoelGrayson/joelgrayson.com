728cd6883466bcf1f3250b0faf8357a044b8ea95
Joel Grayson <joel@joelgrayson.com>
2024-02-02 19:19:26 -0500

    added scores for each instrument in In Like a Lion

--- src/components/page/music/Compositions.tsx
+++ src/components/page/music/Compositions.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 const compositions: [string, string, string][]=[
-    ['In Like a Lion, Out Like a Lamb', '2 Violins, Viola, Cello', '2019'],
+    ['In Like a Lion, Out Like a Lamb', '2 Violins, Viola, Cello', 'Feb 2024'],
     ['The Yearning Soul', '2 Violins', 'August 2022'],
     ['Jogging Fury', 'Violin', 'May 2021'],
     ['Cadenza', 'Violin', 'May 2021'],
@@ -12,8 +12,8 @@ export default function Compositions() {
         <h2>Compositions</h2>
         <div>
             {compositions.map((composition, i)=>{
-                const [name, instrument, date]=composition;
-                return <div key={i} className='grid grid-cols-[1fr_2fr] items-center my-3' style={{
+                const [name, instruments, date]=composition;
+                return <div key={i} className='d:grid d:grid-cols-[1fr_2fr] items-center my-3' style={{
                     border: '2px solid #0080b2',
                     padding: 10,
                     backgroundColor: '#fefefe',
@@ -21,15 +21,24 @@ export default function Compositions() {
                 }}>
                     <div>
                         <p className='bold'>{name}</p>
-                        <p>{instrument}</p>
+                        <p>{instruments}</p>
                         <p>{date}</p>
-                        <Link href={`/music/${name}.pdf`}>Sheet Music</Link>
+                        {
+                            name==='In Like a Lion, Out Like a Lamb'
+                            ? <div>
+                                <span>Sheet Music 🎼: </span>
+                                <Link href={`/music/${name}.pdf`}>All</Link>,&nbsp;
+                                <Link href={`/music/${name}-Violin I.pdf`}>Violin 1</Link>,&nbsp;
+                                <Link href={`/music/${name}-Violin II.pdf`}>Violin 2</Link>,&nbsp;
+                                <Link href={`/music/${name}-Viola.pdf`}>Viola</Link>,&nbsp;
+                                <Link href={`/music/${name}-Cello.pdf`}>Cello</Link>
+                            </div>
+                            : <Link href={`/music/${name}.pdf`}>Sheet Music 🎼</Link>
+                        }
                     </div>
                     <audio
                         src={`/music/${name}.mp3`}
-                        style={{
-                            width: '-webkit-fill-available'
-                        }}
+                        style={{ width: '-webkit-fill-available' }}
                         controls
                     />
                 </div>;
