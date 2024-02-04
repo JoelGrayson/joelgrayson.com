6d9a92d154acc1541d7b9b800ab20c125dde9865
Joel Grayson <joel@joelgrayson.com>
2024-02-04 14:46:20 -0500

    reforming-maths

--- src/components/MathsReturnButton.tsx
+++ src/components/MathsReturnButton.tsx
@@ -1,14 +1,11 @@
-import Button from '@jcomponents/button';
 import Link from 'next/link';
 
 export default function MathsReturnButton() {
-    return <Link href='/maths'>
-        <Button style={{
-            position: 'absolute',
-            top: '1rem',
-            left: '2rem'
-        }}>
-            ⏎ Return to all maths
-        </Button>
+    return <Link href='/maths' className='btn unstyled' style={{
+        position: 'absolute',
+        top: '1rem',
+        left: '2rem'
+    }}>
+        ⏎ Return to all maths
     </Link>;
 }
--- src/pages/maths/reforming-maths/index.tsx
+++ src/pages/maths/reforming-maths/index.tsx
@@ -2,6 +2,8 @@ import MathsReturnButton from 'src/components/MathsReturnButton';
 import Page from '@/components/page/DefaultPage';
 import Script from 'next/script';
 
+const r=String.raw;
+
 export default function ReformingMath() {
     return <Page bottomPadding>
         <MathsReturnButton />
@@ -11,24 +13,25 @@ export default function ReformingMath() {
         <h1 className='text-center text-4xl my-10'>Reforming Math</h1>
         <p>Here are my suggestions for making math notation better:</p>
         <ul>
+            {/* <li>Instead of \(\int\;dx\), use \(dx\over d\). From my basic understanding of calculus, \(\int\) does the same thing as \(\frac1d\). It is confusing to me to have different symbols mean the same thing.</li> */}
             <li>Use \(\tau\) instead of \(\pi\) for the circle constant</li>
-            <li>Instead of writing {String.raw`\(a_{12}\)`} to mean the edge between points \(1\) and \(2\) in graph theory, write {String.raw`\(a_{1\leftrightarrow2}\)`}. Otherwise, it looks like indexing \(12\) of \(a\). When indexing a 2d-array, write {String.raw`\(a_{1,2}\)`} instead of {String.raw`\(a_{12}\)`}. In a 100x100 array, {String.raw`\(a_{12}\)`} should return a 1x100 vector (analogous to a[12]), not a number (analogous to a[1][2])</li>
+            <li>Instead of writing {r`\(a_{12}\)`} to mean the edge between points \(1\) and \(2\) in graph theory, write {r`\(a_{1\leftrightarrow2}\)`}. Otherwise, it looks like indexing \(12\) of \(a\). When indexing a 2d-array, write {r`\(a_{1,2}\)`} instead of {r`\(a_{12}\)`}. In a 100x100 array, {r`\(a_{12}\)`} should return a 1x100 vector (analogous to a[12]), not a number (analogous to a[1][2])</li>
             <li>
-                A big source of confusion in math is not knowing what letter variables stand for. The problem with saying {String.raw`\(\text{charge}=\text{capacitance}\times \text{voltage}\)`} is that it can be interpreted as
+                A big source of confusion in math is not knowing what letter variables stand for. The problem with saying {r`\(\text{charge}=\text{capacitance}\times \text{voltage}\)`} is that it can be interpreted as
                 \(
                     c\times h\times a\times r\times g\times e=c\times a\times p\times a\times c\times i\times t\times a\times n\times c\times e\times v\times o\times l\times t\times a\times g\times e
                 \). The solution is to write a line over multiletter variables (inspired by the usage of the vinculum as a grouping operator).
                 <br />
-                Write {String.raw`\(\overline{\text{charge}}=\overline{\text{capacitance}}\ \overline{\text{voltage}}\)`}
+                Write {r`\(\overline{\text{charge}}=\overline{\text{capacitance}}\ \overline{\text{voltage}}\)`}
                 <br />
                 If you want start the equation with a multiletter variable and use single-letter variables later, you can use \(\rm as\), like so:
                 <br />
-                {String.raw`\[\overline{\text{charge as Q}}=\overline{\text{capacitance as C}}\times\overline{\text{voltage as V}}\]`}
-                {String.raw`\[V=\frac{Q}C\]`}
+                {r`\[\overline{\text{charge as Q}}=\overline{\text{capacitance as C}}\times\overline{\text{voltage as V}}\]`}
+                {r`\[V=\frac{Q}C\]`}
             </li>
             <li>
-                {String.raw`\(\sin^2(x)\)`} should mean {String.raw`\(\sin(\sin(x))\)`} and {String.raw`\(sin^{-1}(x)\)`} should mean \(arcsin(x)\) whereas <br />
-                {String.raw`\(\sin(x)^2\)`} should mean {String.raw`\(\sin(x)\cdot \sin(x)\)`} and {String.raw`\(\sin(x)^{-1}\)`} should mean {String.raw`\(\frac1{\sin(x)}\)`}
+                {r`\(\sin^2(x)\)`} should mean {r`\(\sin(\sin(x))\)`} and {r`\(sin^{-1}(x)\)`} should mean \(arcsin(x)\) whereas <br />
+                {r`\(\sin(x)^2\)`} should mean {r`\(\sin(x)\cdot \sin(x)\)`} and {r`\(\sin(x)^{-1}\)`} should mean {r`\(\frac1{\sin(x)}\)`}
             </li>
             <li>
                 <div>Problem: \((1, π)\) can be a cartesian or polar coordinate{/* or GCD or interval */}.</div>
@@ -37,7 +40,7 @@ export default function ReformingMath() {
                 </div>
             </li>
             <li>
-                <div>Use == when writing a proof, so that you remember that you cannot manipulate both sides like an equation. For example, prove {String.raw`\(\frac{\cos 2x-1}{\sin 2x}==-\tan x\)`}</div>
+                <div>Use == when writing a proof, so that you remember that you cannot manipulate both sides like an equation. For example, prove {r`\(\frac{\cos 2x-1}{\sin 2x}==-\tan x\)`}</div>
             </li>
         </ul>
     </Page>;
