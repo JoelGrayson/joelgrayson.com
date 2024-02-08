3afa9dd6c6a1dbf8efb1860fdf20a5651bd88b3d
Joel Grayson <joel@joelgrayson.com>
2024-02-08 06:33:13 -0500

    updated /software/edit-time

--- src/pages/software/edit-time/index.tsx
+++ src/pages/software/edit-time/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Page from '@/components/page/DefaultPage';
+import Link from 'next/link';
 // import Button from '@jcomponents/button';
 
 export default function EditTime() {
@@ -7,10 +8,20 @@ export default function EditTime() {
         title: 'Edit Time',
         description: 'The Mac app for changing a file\'s created and modified dates',
     }}>
-        <h1 className='text-center'>Edit Time</h1>
+        <h1 className='text-center flex items-center justify-center gap-3'>
+            <Image src='/image/software/edit-time/logo.png' width={60} height={60} alt='Logo' className='inline relative bottom-1' />
+            Edit Time
+        </h1>
         <p className='italic text-center text-gray-400'>The Mac app for changing a file&apos;s created and modified dates</p>
         <br />
-        <p>Available on the Mac App Store (coming soon)</p>
+        {/* <p>Available on the Mac App Store</p> */}
+        <Link target='_blank' href='https://apps.apple.com/us/app/edit-time/id6464405876'>
+            <Image src="/image/software/edit-time/download-on-the-mac-app-store.png" alt="Download on the Mac App Store" height={74.375} width={170} style={{
+                position: 'absolute',
+                right: '40px',
+                top: '130px'
+            }} />
+        </Link>
         {/* <a className='button' href=''>Download for Mac</a> */}
         <br />
 
@@ -30,7 +41,7 @@ export default function EditTime() {
 
         <h3>Demo</h3>
         {/* <Image src="/software/edit-time/demo.gif" alt="Demo" width='554' height='338' /> */}
-        <video src="https://s3.amazonaws.com/joelgrayson.com/Edit+Time+Demo.mp4" controls autoPlay></video>
+        <video src="https://s3.amazonaws.com/joelgrayson.com/Edit+Time+Demo.mp4" controls autoPlay muted></video>
 
         <br />
         <h3>Support/Bugs</h3>
