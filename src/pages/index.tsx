1c8f9f7c86af8b16d283252af374033889d4eab0
Joel Grayson <joel@joelgrayson.com>
2024-02-26 19:01:27 -0500

    added description to main site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import Page from '@/components/page/DefaultPage';
 import Content from '../app/Content';
+import Link from 'next/link';
 
 export default function Home() {
-    return <Page noPadding pathname='/' seo={{
+    return <Page noPadding bottomPadding pathname='/' seo={{
         title: 'Joel Grayson',
         keywords: [
             'Joel Grayson',
@@ -48,8 +49,18 @@ export default function Home() {
             </div>
         </div>
 
+        <div className="w-full max-w-[650px] mx-auto pt-8 pb-1 leading-8">
+            <p className='indent-8'>Hi, I&apos;m Joel. I have a passion for <Link href='/combating-climate-change'>combating climate change</Link>, <Link href='/nyc'>government</Link>, <Link href='/software'>software development</Link>, and <Link href='/machines'>engineering</Link>.</p>
+            <p className='indent-8'>I am working to bring a <Link href='/combating-climate-change#solar-for-riverdale'>solar installation</Link> to Riverdale Country School, which will save 331 metric tons of CO₂ equivalent each year, save Riverdale money, supply backup power during outages, and provide live energy data for use in the science curriculum. I founded <Link href='https://studentsforelectricbuses.org' target='_blank'>Students for Electric Buses</Link>, a club attempting to transition Riverdale’s bus provider to electric school buses. I also did <Link href='/research/organic-optoelectronics'>research on organic solar cells</Link> at New York University’s Lee Lab.</p>
+            <p className='indent-8'>I serve on Manhattan Community Board One’s Environmental Protection Committee, which plans Lower Manhattan’s climate resiliency projects, and the Youth & Education Committee. I am working with the Mayor’s Office of Climate and Environmental Justice and Street Vendor Project to enable street vendors to connect to the electrical grid instead of using gas generators, which would prevent 450 tons of gas from being burned annually per vendor and improve NYC’s air quality (<Link href='/connecting-street-vendors-to-the-grid'>see more</Link>). I am working to open new maker spaces in public schools to give kids the same opportunity to make that I had.</p>
+            <p className='indent-8'>Among my software projects, <Link href='https://buseroo.com' target='_blank'>Buseroo.com</Link> has helped students find which school bus goes to any address over 500 times, <Link href='https://chromewebstore.google.com/detail/homework-checker-schoolog/aflepcmbhmafadnddmdippaajhjnmohj' target='_blank'>Homework Checker</Link> helps 1,000 students manage their homework, <Link href='https://chromewebstore.google.com/detail/focus-for-google-docs/djnloioaddlnmagobbcnjpppmbelfocf' target='_blank'>Focus for Google Docs</Link> helps over 400 people write without distractions, and <Link href='https://lirongart.com' target='_blank'>LirongArt.com</Link> showcases my mom’s paintings.</p>
+            <p className='indent-8'>Among my engineering projects, I made a <Link href='https://youtu.be/8BwmKFodra0?t=76' target='_blank'>Jacob’s ladder</Link> that acts as my alarm, waking me up every morning. I built a vending machine that serves snacks to people at school, a wooden pinball machine, Tesla coil, and various other devices. You can explore my projects in more depth below:</p>
+        </div>
+
+
         {/* Client component */}
         <Content />
+
     </Page>;
 }
 
