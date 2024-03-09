a3f8a2e63f400cfe3f2f44a1c451601f80086a4e
Joel Grayson <joel@joelgrayson.com>
2024-03-09 16:10:51 -0500

    trying to fix CORS error

--- next.config.js
+++ next.config.js
@@ -48,7 +48,7 @@ const nextConfig={
             }
         ];
 
-        function CORSHeaders(origin='*', methods='GET, POST, PUT') {
+        function CORSHeaders(origin='*', methods='GET, POST, PUT, PATCH, OPTIONS') {
             return [
                 { key: "Access-Control-Allow-Credentials", value: "true" },
                 { key: "Access-Control-Allow-Origin", value: origin }, //the origin
