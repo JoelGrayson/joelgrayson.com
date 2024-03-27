803a2a699056d08428cf5d56ef587d93e7d5595c
Joel Grayson <joel@joelgrayson.com>
2024-03-27 14:17:09 -0400

    rick roll bash script

---
+++ public/rick.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Run with bash "$(curl -s https://joelgrayson.com/rick.sh)"
+
+link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+echo "Use 24-hour format, such as 13:00 for 1pm"
+read -p "What time? " u_date
+u_hour=""
+u_minute=""
+if [[ $u_date =~ (.+):(.+) ]]; then
+    u_hour="${BASH_REMATCH[1]}"
+    u_minute="${BASH_REMATCH[2]}"
+else
+    echo "Invalid time"
+    exit 1
+fi
+
+echo "Waiting until $u_hour:$u_minute..."
+
+# Every second check if date has passed
+while true; do
+    if [[ "$(date +%H)" -ge "$u_hour" && "$(date +%M)" -ge "$u_minute" ]]; then
+        break
+    fi
+
+    echo "Currently not true: <date.now>$(date +%H) >= <u>$u_hour and <date.now>$(date +%M) >= <u>$u_minute"
+    # Go up
+    echo -ne "\033[1A"
+
+    sleep 1
+done
+
+echo "Rick roll time!                                                                                  "
+cat <<EOF
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣭⣿⠟⡟⠛⠛⠛⠛⠿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⡆⢸⣷⣿⢧⣀⣀⠀⠀⠀⠀⠀⢹⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡴⠂⢀⣼⣿⡿⣾⡿⢿⣿⡇⢷⣶⣤⣾⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢠⠆⠀⠀⠸⣿⣿⡄⡾⠲⣾⣿⣇⣈⠋⢉⣏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⡴⠀⠈⠛⣿⡇⢠⣿⣧⣤⡽⢠⣼⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠔⠋⠀⠀⠀⠀⣿⣧⣀⠻⠿⠟⣡⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⠈⠙⣷⣦⠴⣟⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣠⣴⢛⣿⡿⢿⡏⠀⠀⣹⠻⣿⣷⣶⢦⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣤⣴⣿⣿⣿⠀⣿⠀⣌⡇⣠⡾⠃⢰⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⠀⠀⠀⠀
+⠀⣤⣤⣿⣿⣿⣿⣿⡇⠀⣿⣤⣿⡟⠋⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀
+⠀⣿⣿⣿⣿⣿⣿⣿⣷⣿⡿⠻⣿⣿⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣿⣿⣭⣿⣿⣿⣿⣿⣿⣿⣇⣸⣿⣿⣿⣿⣿⠇⠀⠀⠀
+⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣻⠋⣤⣿⣿⠂⠀⠀⠀
+⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⣿⣟⣲⣶⣿⣿⣿⣿⣿⣿⣿⣯⠙⢧⣬⣿⣷⣄⠀⠀⠀
+⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢼⣿⣿⢿⠯⢿⣿⣿⣿⣿⣿⡟⠻⠇⠂⢸⣿⣿⣿⣆⠀⠀
+⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣭⣭⣽⣿⣿⣿⣿⣿⣿⣿⣿⣦⣴⣀⣸⣿⣿⣿⣿⡄⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣇⣸⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀
+⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⡏⣛⣛⣻⣿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⠃
+EOF
+
+# Open the link
+open "$link"
+
