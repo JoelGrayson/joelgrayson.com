7f8769be366e1bf864754b0f8e2dc468b732dfcf
Joel Grayson <joel@joelgrayson.com>
2024-08-06 13:07:52 -0400

    /resume: make titles and description able to take up more room. Only don't cover the date area.

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -39,10 +39,12 @@ export default function Resume() {
             }
             #root .title {
                 display: inline-block;
-                max-width: calc(100% - 4rem /* size of year */);
             }
-            #root .title.with-icon {
-                max-width: calc(100% - 6rem /* size of year */);
+            #root .title::before, .description.is-title::before {
+                content: '';
+                float: right;
+                width: 4rem;
+                height: 1rem;
             }
             #root li>img {
                 vertical-align: top;
@@ -54,13 +56,13 @@ export default function Resume() {
                 font-size: 0.9rem;
                 // no bullet point
                 list-style-type: none;
-                max-width: calc(100% - 2rem);
-            }
-            #root .activities .description {
-                max-width: calc(100% - 4.5rem); //in activities, there are no titles, so the dates overlap with the descriptions
             }
             #root .indent {
                 padding-left: 3rem;
+            }
+            #root .inline-description {
+                position: absolute;
+                right: 4.5rem;
             }
 
             #root .section {
@@ -69,30 +71,6 @@ export default function Resume() {
                 padding-top: 4px;
                 border-radius: 8px;
             }
-
-            // @media (max-width: 600px) {
-            //     // Mobile
-            //     #root .title {
-            //         display: initial;
-            //     }
-            //     #root .date {
-            //         display: inline-block;
-            //         // text-align: right;
-            //         position: initial;
-            //         padding-left: 1rem;
-            //         white-space: nowrap;
-            //     }
-            //     #root .indent {
-            //         padding-left: 1rem;
-            //     }
-            //     #root h1 {
-            //         padding-top: 5.5rem;
-            //         font-size: 2rem;
-            //     }
-            //     #root .description {
-            //         max-width: none;
-            //     }
-            // }
         `}>
             <div id="root" className="j_container relative flex flex-col">
                 <h1 className='text-center text-[46px] pt-4'>Joel Grayson</h1>
@@ -121,30 +99,38 @@ export default function Resume() {
                     <h4>Climate Change</h4>
                     <ul>
                         <li>
-                            <Image src='/image/ccc/solar-for-riverdale/solar-panel.png' alt='solar panel' width={26} height={20}
-                                className='inline bottom-1 relative'
-                                style={{ right: 4 }}
-                            />
-                            <span className="title with-icon">Brought 410 kW solar installation to Riverdale, enough to power 40 households.</span>    
+                            <span className="title with-icon">
+                                <Image src='/image/ccc/solar-for-riverdale/solar-panel.png' alt='solar panel' width={26} height={20}
+                                    className='inline bottom-0.5 relative'
+                                    style={{ right: 4 }}
+                                />
+                                Brought 410 kW solar installation to Riverdale, enough to power 40 households.
+                            </span>    
                             <span className="date">2021&ndash;2024</span>
                             <div className="description indent">Collected rooftop measurements, created solar models in HelioScope, gathered quotes from several installers, researched legal and financial aspects, and presented to administration and Board of Trustees.</div>
                         </li>
                         <li className='mb-2'>
-                            <SEBLogo size={25} inline style={{ marginLeft: 4, marginRight: -4, top: 7, right: 4, verticalAlign: 'top', paddingTop: 7 }} />
-                            <span className="title with-icon">Founded <Link href='https://studentsforelectricbuses.org'>Students for Electric Buses</Link> to electrify bus fleets.</span>
+                            <span className="title with-icon">
+                                <SEBLogo size={25} inline style={{ marginLeft: 4, marginRight: -4, top: 7, right: 4, paddingTop: 4 }} />
+                                Founded <Link href='https://studentsforelectricbuses.org'>Students for Electric Buses</Link> to electrify bus fleets.
+                            </span>
                             <span className="date !top-[5px]">2021&ndash;2024</span>
                         </li>
                         <li>
-                            <Image src='/image/ccc/electric-leaf-blowers/electric-leaf-blower.png' alt='leaf blower' width={20} height={13} className='relative bottom-[3px] mr-1 inline' />
-                            <span className="title with-icon">Electric Leaf Blowers</span>
+                            <span className="title with-icon">
+                                <Image src='/image/ccc/electric-leaf-blowers/electric-leaf-blower.png' alt='leaf blower' width={20} height={13} className='relative bottom-[3px] mr-1 inline' />
+                                Electric Leaf Blowers
+                            </span>
                             <span className="date">2022</span>
                             <div className='description indent'>Convinced my school&apos;s facilities team to switch from gas to electric leaf blowers.</div>
                         </li>
                         <li>
-                            <Image src='/image/ccc/stopping-bus-idling/stopping-bus-idling.png' alt='stopping-bus-idling' width={20} height={13} className='relative bottom-[3px] mr-1 inline' />
-                            <span className="title">Stopping Bus Idling</span>
+                            <span className="title">
+                                <Image src='/image/ccc/stopping-bus-idling/stopping-bus-idling.png' alt='stopping-bus-idling' width={20} height={13} className='relative bottom-[3px] mr-1 inline' />
+                                Stopping Bus Idling
+                            </span>
                             <span className="date">2022</span>
-                            <div className="description indent">Wrote to bus contractor to enforce <Link href='https://portal.311.nyc.gov/article/?kanumber=KA-02222'>NYC&apos;s law banning bus idling</Link>.</div>
+                            <div className="description inline-description">Wrote to bus contractor to enforce <Link href='https://portal.311.nyc.gov/article/?kanumber=KA-02222'>NYC&apos;s law banning bus idling</Link>.</div>
                         </li>
                         <li className='mb-2 mt-2'>
                             <span className="title">Presented to middle school, upper school, and Riverdale alumni on climate solutions.</span>
@@ -165,34 +151,41 @@ export default function Resume() {
                     <h4>Software</h4>
                     <ul>
                         <li>
-                            <Image src='/image/home/buseroo-logo.png' alt='buseroo-logo' width={26} height={26} className='mr-1 inline bottom-[3px] right-[3px] relative' />
-                            <span className="title with-icon"><Link href="https://buseroo.com">Buseroo.com</Link></span>
+                            <span className="title with-icon">
+                                <Image src='/image/home/buseroo-logo.png' alt='buseroo-logo' width={26} height={26} className='mr-1 inline bottom-[3px] right-[3px] relative' />
+                                <Link href="https://buseroo.com">Buseroo.com</Link>
+                            </span>
                             <span className="date">2021&ndash;23</span>
-                            <div className="description indent">kiosk and website for students to find closest bus to any address.</div>
+                            <div className="description inline-description">kiosk and website for students to find closest bus to any address.</div>
                         </li>
                         <li>
-                            <Image src='/image/home/homework-checker-logo.png' alt='homework-checker-logo' width={26} height={26} className='mr-1 inline bottom-[3px] right-[3px] relative' />
-                            <span className="title with-icon"><Link href='https://chromewebstore.google.com/detail/homework-checker-schoolog/aflepcmbhmafadnddmdippaajhjnmohj'>Homework Checker</Link></span>
+                            <span className="title with-icon">
+                                <Image src='/image/home/homework-checker-logo.png' alt='homework-checker-logo' width={26} height={26} className='mr-1 inline bottom-[3px] right-[3px] relative' />
+                                <Link href='https://chromewebstore.google.com/detail/homework-checker-schoolog/aflepcmbhmafadnddmdippaajhjnmohj'>Homework Checker</Link>
+                            </span>
                             <span className="date">2021&ndash;22</span>
-                            <div className="description indent">Chrome extension helping 1000 students manage homework.</div>
+                            <div className="description inline-description">Chrome extension helping 1000 students manage homework.</div>
                         </li>
                         <li>
-                            <Image src='/image/home/focus-logo.png' alt='focus-logo' width={26} height={26} className='mr-1 inline bottom-[3px] right-[3px] relative' />
-                            <span className="title with-icon"><Link href="https://chromewebstore.google.com/detail/focus-for-google-docs/djnloioaddlnmagobbcnjpppmbelfocf">Focus</Link></span>
+                            <span className="title with-icon">
+                                <Image src='/image/home/focus-logo.png' alt='focus-logo' width={26} height={26} className='mr-1 inline bottom-[3px] right-[3px] relative' />
+                                <Link href="https://chromewebstore.google.com/detail/focus-for-google-docs/djnloioaddlnmagobbcnjpppmbelfocf">Focus</Link>
+                            </span>
                             <span className="date">2022</span>
-                            <div className="description indent">Chrome extension helping 400+ people write distraction-free.</div>
+                            <div className="description inline-description">Chrome extension helping 400+ people write distraction-free.</div>
                         </li>
                         <li>
-                            <Image src='/image/lirong-art/lirong-art.png' alt='lirongart-logo' width={16.8125} height={30} className='mr-1 inline bottom-[3px] right-[3px] relative' />
-                            <span className="title with-icon"><Link href='https://lirongart.com'>LirongArt.com</Link></span>
+                            <span className="title with-icon">
+                                <Image src='/image/lirong-art/lirong-art.png' alt='lirongart-logo' width={16.8125} height={30} className='mr-1 inline bottom-[3px] right-[3px] relative' />
+                                <Link href='https://lirongart.com'>LirongArt.com</Link>
+                            </span>
                             <span className="date">2020</span>
-                            <div className="description indent">online gallery showcasing my mom’s paintings.</div>
+                            <div className="description inline-description">online gallery showcasing my mom’s paintings.</div>
                         </li>
                         <li>
-                            {/* TODO: icon */}
-                            <span className="title">&emsp;&nbsp;Download link generators</span>
+                            <span className="title">Download link generators</span>
                             <span className="date">2021</span>
-                            <div className="description indent">Tools for cloud storage used 17,000 times.</div>
+                            <div className="description inline-description">Tools for cloud storage used 17,000 times.</div>
                         </li>
                     </ul>
                 </div>
@@ -211,45 +204,45 @@ export default function Resume() {
                 <div className="section hover:bg-red-100 activities">
                     <h3>Activities</h3>
                     <div className='relative'>
-                        <span className='description'>Manhattan Community Board 1 member: Environmental and Youth & Ed Committees.</span>
+                        <span className='description is-title'>Manhattan Community Board 1 member: Environmental and Youth & Ed Committees.</span>
                         <span className='date'>2023&ndash;24</span>
                     </div>
                     <div className='relative'>
-                        <span className='description'>Researched novel compound at Lee Lab in NYU&apos;s seven-week ARISE program.</span>
+                        <span className='description is-title'>Researched novel compound at Lee Lab in NYU&apos;s seven-week ARISE program.</span>
                         <span className='date relative'>2023</span>
                     </div>
                     <div className='relative'>
-                        <span className='description'>Grade representative for five years in student government.</span>
+                        <span className='description is-title'>Grade representative for five years in student government.</span>
                         <span className='date'>2019&ndash;now</span>
                     </div>
                     <div className='relative'>
-                        <span className='description'>Completed MIT xPRO&apos;s Introduction to Quantum Computing four-week course.</span>
+                        <span className='description is-title'>Completed MIT xPRO&apos;s Introduction to Quantum Computing four-week course.</span>
                         <span className='date'>2023</span>
                     </div>
                     <div className='relative'>
-                        <span className='description'>Interned for the Stone House Group (energy efficiency consultants).</span>
+                        <span className='description is-title'>Interned for the Stone House Group (energy efficiency consultants).</span>
                         <span className='date'>2021</span>
                     </div>
                     <div className='relative'>
-                        <span className='description'>Summer courses: NY Math Circle, Center for Talented Youth, Brown, Tufts, BU.</span>
+                        <span className='description is-title'>Summer courses: NY Math Circle, Center for Talented Youth, Brown, Tufts, BU.</span>
                         <span className='date'>2015&ndash;22</span>
                     </div>
                     <div className='relative'>
-                        <span className='description'>Violin: Praeludium & Allegro, Spring, Bach&apos;s Concerto No. 1 recitals; Interlochen; <Link href='/music'>Compositions</Link>.</span>
+                        <span className='description is-title'>Violin: Praeludium & Allegro, Spring, Bach&apos;s Concerto No. 1 recitals; Interlochen; <Link href='/music'>Compositions</Link>.</span>
                         <span className='date'>2011&ndash;now</span>
                     </div>
                     <div className='relative'>
-                        <span className='description'><Link href='https://youtube.com/SonAndFather'>YouTube channel</Link> with father-son comedy, project records, and claymation.</span>
+                        <span className='description is-title'><Link href='https://youtube.com/SonAndFather'>YouTube channel</Link> with father-son comedy, project records, and claymation.</span>
                         {/* Wrote, acted, filmed, and edited 34 comedy videos with my father since age 11, created nine9 stop motion animations, and included other projects */}
                         <span className='date'>2018&ndash;now</span>
                     </div>
                 </div>
 
 
-                <div className="section hover:bg-yellow-100">
+                <div className="section hover:bg-yellow-100 honors">
                     <h3>Honors</h3>
                     <div className='relative'>
-                        <span>Finalist in the Rise competition (global search for talented youth).</span>
+                        <span className="description is-title">Finalist in the Rise competition (global search for talented youth).</span>
                         <span className='date'>2021&ndash;22</span>
                     </div>
                 </div>            
