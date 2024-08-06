75dd064bc883a307eaf24519ef1950496c150b5d
Joel Grayson <joel@joelgrayson.com>
2024-08-06 13:17:50 -0400

    /resume: added spacing

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -30,6 +30,7 @@ export default function Resume() {
                 position: relative;
                 list-style-type: none;
                 margin-left: 0;
+                margin-bottom: 3px;
             }
             #root .date {
                 position: absolute;
@@ -71,6 +72,13 @@ export default function Resume() {
                 padding-top: 4px;
                 border-radius: 8px;
             }
+
+            #software-section li {
+                margin-block: .4rem;
+            }
+            #education-section li {
+                margin-block: .4rem;
+            }
         `}>
             <div id="root" className="j_container relative flex flex-col">
                 <h1 className='text-center text-[46px] pt-4'>Joel Grayson</h1>
@@ -95,7 +103,7 @@ export default function Resume() {
 
                 <h3>Accomplishments</h3>
 
-                <div className="section hover:bg-green-100">
+                <div className="section hover:bg-green-100" id='climate-change-section'>
                     <h4>Climate Change</h4>
                     <ul>
                         <li>
@@ -147,7 +155,7 @@ export default function Resume() {
                     </ul>
                 </div>
                 
-                <div className="section hover:bg-blue-100">
+                <div className="section hover:bg-blue-100" id='software-section'>
                     <h4>Software</h4>
                     <ul>
                         <li>
@@ -176,7 +184,7 @@ export default function Resume() {
                         </li>
                         <li>
                             <span className="title with-icon">
-                                <Image src='/image/lirong-art/lirong-art.png' alt='lirongart-logo' width={16.8125} height={30} className='mr-1 inline bottom-[3px] right-[3px] relative' />
+                                <Image src='/image/lirong-art/lirong-art.png' alt='lirongart-logo' width={16.8125} height={30} className='ml-[4.6px] mr-[8.5px] inline bottom-[3px] right-[3px] relative' />
                                 <Link href='https://lirongart.com'>LirongArt.com</Link>
                             </span>
                             <span className="date">2020</span>
@@ -190,61 +198,73 @@ export default function Resume() {
                     </ul>
                 </div>
 
-                <div className="section hover:bg-gray-200">
+                <div className="section hover:bg-gray-200" id="education-section">
                     <h3>Education</h3>
-                    <div className='relative'>
-                        <span>Riverdale Country School</span>
-                        <span className='date'>2017&ndash;24</span>
-                    </div>
-                    <div>Honors: Geometry, Algebra II, Precalculus, Spanish I&ndash;VII, Biology II, Chemistry II, Physics II</div>
-                    <div>GPA: 3.94 (out of 4.0), SAT: 1560</div>
+                    <ul>
+                        <li>
+                            <span>Stanford University</span>
+                            <span className='date'>2024&ndash;28</span>
+                        </li>
+                        <li>
+                            <span>Riverdale Country School</span>
+                            <span className='date'>2017&ndash;24</span>
+                            <div className="description indent">
+                                <div>Honors: Geometry, Algebra II, Precalculus, Spanish I&ndash;VII, Biology II, Chemistry II, Physics II</div>
+                                <div>GPA: 3.94 (out of 4.0), SAT: 1560</div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
 
 
-                <div className="section hover:bg-red-100 activities">
+                <div className="section hover:bg-red-100" id='activities-section'>
                     <h3>Activities</h3>
-                    <div className='relative'>
-                        <span className='description is-title'>Manhattan Community Board 1 member: Environmental and Youth & Ed Committees.</span>
-                        <span className='date'>2023&ndash;24</span>
-                    </div>
-                    <div className='relative'>
-                        <span className='description is-title'>Researched novel compound at Lee Lab in NYU&apos;s seven-week ARISE program.</span>
-                        <span className='date relative'>2023</span>
-                    </div>
-                    <div className='relative'>
-                        <span className='description is-title'>Grade representative for five years in student government.</span>
-                        <span className='date'>2019&ndash;now</span>
-                    </div>
-                    <div className='relative'>
-                        <span className='description is-title'>Completed MIT xPRO&apos;s Introduction to Quantum Computing four-week course.</span>
-                        <span className='date'>2023</span>
-                    </div>
-                    <div className='relative'>
-                        <span className='description is-title'>Interned for the Stone House Group (energy efficiency consultants).</span>
-                        <span className='date'>2021</span>
-                    </div>
-                    <div className='relative'>
-                        <span className='description is-title'>Summer courses: NY Math Circle, Center for Talented Youth, Brown, Tufts, BU.</span>
-                        <span className='date'>2015&ndash;22</span>
-                    </div>
-                    <div className='relative'>
-                        <span className='description is-title'>Violin: Praeludium & Allegro, Spring, Bach&apos;s Concerto No. 1 recitals; Interlochen; <Link href='/music'>Compositions</Link>.</span>
-                        <span className='date'>2011&ndash;now</span>
-                    </div>
-                    <div className='relative'>
-                        <span className='description is-title'><Link href='https://youtube.com/SonAndFather'>YouTube channel</Link> with father-son comedy, project records, and claymation.</span>
-                        {/* Wrote, acted, filmed, and edited 34 comedy videos with my father since age 11, created nine9 stop motion animations, and included other projects */}
-                        <span className='date'>2018&ndash;now</span>
-                    </div>
+                    <ul>
+                        <li>
+                            <span className='description is-title'>Manhattan Community Board 1 member: Environmental and Youth & Ed Committees.</span>
+                            <span className='date'>2023&ndash;24</span>
+                        </li>
+                        <li>
+                            <span className='description is-title'>Researched novel compound at Lee Lab in NYU&apos;s seven-week ARISE program.</span>
+                            <span className='date relative'>2023</span>
+                        </li>
+                        <li>
+                            <span className='description is-title'>Grade representative for five years in student government.</span>
+                            <span className='date'>2019&ndash;now</span>
+                        </li>
+                        <li>
+                            <span className='description is-title'>Completed MIT xPRO&apos;s Introduction to Quantum Computing four-week course.</span>
+                            <span className='date'>2023</span>
+                        </li>
+                        <li>
+                            <span className='description is-title'>Interned for the Stone House Group (energy efficiency consultants).</span>
+                            <span className='date'>2021</span>
+                        </li>
+                        <li>
+                            <span className='description is-title'>Summer courses: NY Math Circle, Center for Talented Youth, Brown, Tufts, BU.</span>
+                            <span className='date'>2015&ndash;22</span>
+                        </li>
+                        <li>
+                            <span className='description is-title'>Violin: Praeludium & Allegro, Spring, Bach&apos;s Concerto No. 1 recitals; Interlochen; <Link href='/music'>Compositions</Link>.</span>
+                            <span className='date'>2011&ndash;now</span>
+                        </li>
+                        <li>
+                            <span className='description is-title'><Link href='https://youtube.com/SonAndFather'>YouTube channel</Link> with father-son comedy, project records, and claymation.</span>
+                            {/* Wrote, acted, filmed, and edited 34 comedy videos with my father since age 11, created nine9 stop motion animations, and included other projects */}
+                            <span className='date'>2018&ndash;now</span>
+                        </li>
+                    </ul>
                 </div>
 
 
-                <div className="section hover:bg-yellow-100 honors">
+                <div className="section hover:bg-yellow-100" id='honors-section'>
                     <h3>Honors</h3>
-                    <div className='relative'>
-                        <span className="description is-title">Finalist in the Rise competition (global search for talented youth).</span>
-                        <span className='date'>2021&ndash;22</span>
-                    </div>
+                    <ul>
+                        <li className='relative'>
+                            <span className="description is-title">Finalist in the Rise competition (global search for talented youth).</span>
+                            <span className='date'>2021&ndash;22</span>
+                        </li>
+                    </ul>
                 </div>            
             </div>
 
