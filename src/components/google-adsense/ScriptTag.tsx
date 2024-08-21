fdf936e61b161dc50194ff0200c14f9852ea75fe
Joel Grayson <joel@joelgrayson.com>
2024-08-20 23:40:59 -0400

    attempting to fix adsense

--- src/components/google-adsense/ScriptTag.tsx
+++ src/components/google-adsense/ScriptTag.tsx
@@ -1,7 +1,11 @@
-import Script from "next/script";
+import Head from "next/head";
+// import Script from "next/script";
 
 export default function GoogleAdsenseScriptTag() {
-    return <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3254188827517112"
-    crossOrigin="anonymous"></Script>;
+    return <Head>
+        {/* <script data-ad-client="ca-pub-3254188827517112" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
+        <div dangerouslySetInnerHTML={{
+            __html: '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3254188827517112" crossorigin="anonymous"></script>'
+        }}></div>
+    </Head>;
 }
-
