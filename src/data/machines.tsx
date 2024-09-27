bfc166f3bd7efb2ee673279eda2c166e1e6bcee0
Joel Grayson <joel@joelgrayson.com>
2024-09-27 08:21:23 -0700

    added Venderoo to /machines

--- src/data/machines.tsx
+++ src/data/machines.tsx
@@ -8,7 +8,19 @@ export const machines: Machine[]=[
     {
         sortBy: {
             rank: 3, //sort by best project
-            date: new Date('July 15, 2021'), //sort by date of project
+            date: new Date('May 2024'), //sort by date of project
+            title: 'Buseroo Kiosk' //sort alphabetically
+        },
+        html: DefaultTemplate({
+            date: '2024',
+            title: <>Venderoo</>,
+            yt: 'bHHk2FL5Ujs'
+        })
+    },
+    {
+        sortBy: {
+            rank: 3, //sort by best project
+            date: new Date('2023'), //sort by date of project
             title: 'Buseroo Kiosk' //sort alphabetically
         },
         html: DefaultTemplate({
@@ -25,11 +37,11 @@ export const machines: Machine[]=[
     {
         sortBy: {
             rank: 3, //sort by best project
-            date: new Date('July 15, 2021'), //sort by date of project
+            date: new Date('Oct 3, 2023'), //sort by date of project
             title: '3D-Printed Chess Board' //sort alphabetically
         },
         html: DefaultTemplate({
-            date: '10.11.2023',
+            date: '10.3.2023',
             title: <>3D-Printed Chess Board</>,
             desc: <>
                 <div className='d:flex d:gap-9'>
@@ -49,11 +61,11 @@ export const machines: Machine[]=[
     {
         sortBy: {
             rank: 3, //sort by best project
-            date: new Date('July 15, 2021'), //sort by date of project
+            date: new Date('Sept 20, 2021'), //sort by date of project
             title: 'Energy T-Shirt' //sort alphabetically
         },
         html: DefaultTemplate({
-            date: '7.15.2021',
+            date: '9.20.2021',
             title: <>Energy T-Shirt</>,
             desc: <>
                 <div className='d:flex d:gap-9'>
@@ -134,10 +146,10 @@ export const machines: Machine[]=[
                     <div className='w-[80%]'>
                         <div>This project depicts the dystopian world many people imagine at the thought of nuclear energy.
                         As a nuclear proponent myself (I wrote an article on my thoughts here), I wanted to explore the fear.</div>
-                        <div>Website: <Link  href='https://joelgrayson.github.io/Nuclear-Dystopia'>joelgrayson.github.io/Nuclear-Dystopia</Link></div>
-                        <div>Code: <Link  href='https://github.com/JoelGrayson/Guardbox'>github.com/JoelGrayson/Guardbox</Link></div>
+                        <div>Website: <Link href='https://joelgrayson.github.io/Nuclear-Dystopia'>joelgrayson.github.io/Nuclear-Dystopia</Link></div>
+                        <div>Code: <Link href='https://github.com/JoelGrayson/Guardbox'>github.com/JoelGrayson/Guardbox</Link></div>
                     </div>
-                    <Link  href='https://joelgrayson.github.io/Nuclear-Dystopia/' target='_blank'>
+                    <Link href='https://joelgrayson.github.io/Nuclear-Dystopia/' target='_blank'>
                         <Image width='170' height='154' className='hover:shadow-xl hover:outline-2 outline-double transition-none outline-0' src='/image/machines/nuclear-dystopia-thumbnail.jpg' alt='Thumbnail' />
                     </Link>
                 </div>
