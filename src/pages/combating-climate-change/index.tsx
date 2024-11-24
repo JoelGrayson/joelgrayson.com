42413af4f9d8ed698feaa82a709175f0276e47f3
Joel Grayson <joel@joelgrayson.com>
2024-11-24 00:47:19 -0800

    dark mode for /ccc

--- src/pages/combating-climate-change/index.tsx
+++ src/pages/combating-climate-change/index.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import styles from '@/styles/ccc/sections.module.css';
 import Link from 'next/link';
 import { Section, Action } from '@/components/by-page/combating-climate-change/helpers';
-import Button from '@jcomponents/button';
 import IdlingEngine from '@/components/by-page/combating-climate-change/IdlingEngine';
 import PDF from '@/components/global/PDF';
 import { Switch } from 'antd';
@@ -180,11 +179,9 @@ Permanent link: https://perma.cc/N7QN-QDK6
             </Action>
             <Action title='Upper School Climate Solutions Presentation' {...{showDescription}}>
                 <div className='flex justify-around items-center'>
-                    <Link className='m:hidden' href='/combating-climate-change/Climate Solutions for Riverdale.pptx'>
-                        <Button>
-                            <svg className={Button.Icon} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48' width='48px' height='48px'><path fill='#dc4c2c' d='M8,24c0,9.941,8.059,18,18,18s18-8.059,18-18H26H8z'/><path fill='#f7a278' d='M26,6v18h18C44,14.059,35.941,6,26,6z'/><path fill='#c06346' d='M26,6C16.059,6,8,14.059,8,24h18V6z'/><path fill='#9b341f' d='M22.319,34H5.681C4.753,34,4,33.247,4,32.319V15.681C4,14.753,4.753,14,5.681,14h16.638 C23.247,14,24,14.753,24,15.681v16.638C24,33.247,23.247,34,22.319,34z'/><path fill='#fff' d='M14.673,19.012H10v10h2.024v-3.521H14.3c1.876,0,3.397-1.521,3.397-3.397v-0.058 C17.697,20.366,16.343,19.012,14.673,19.012z M15.57,22.358c0,0.859-0.697,1.556-1.556,1.556h-1.99v-3.325h1.99 c0.859,0,1.556,0.697,1.556,1.556V22.358z'/></svg>
-                            <span>Download Presentation</span>
-                        </Button>
+                    <Link className='button m:hidden' href='/combating-climate-change/Climate Solutions for Riverdale.pptx'>
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48' width='48px' height='48px'><path fill='#dc4c2c' d='M8,24c0,9.941,8.059,18,18,18s18-8.059,18-18H26H8z'/><path fill='#f7a278' d='M26,6v18h18C44,14.059,35.941,6,26,6z'/><path fill='#c06346' d='M26,6C16.059,6,8,14.059,8,24h18V6z'/><path fill='#9b341f' d='M22.319,34H5.681C4.753,34,4,33.247,4,32.319V15.681C4,14.753,4.753,14,5.681,14h16.638 C23.247,14,24,14.753,24,15.681v16.638C24,33.247,23.247,34,22.319,34z'/><path fill='#fff' d='M14.673,19.012H10v10h2.024v-3.521H14.3c1.876,0,3.397-1.521,3.397-3.397v-0.058 C17.697,20.366,16.343,19.012,14.673,19.012z M15.57,22.358c0,0.859-0.697,1.556-1.556,1.556h-1.99v-3.325h1.99 c0.859,0,1.556,0.697,1.556,1.556V22.358z'/></svg>
+                        <span>Download Presentation</span>
                     </Link>
                     <div className='m:hidden'>
                         <PDF src='combating-climate-change/Climate Solutions for Riverdale.pdf' height={200} />
@@ -195,11 +192,9 @@ Permanent link: https://perma.cc/N7QN-QDK6
                 <div className='d:flex'>
                     <div className='d:grid d:place-items-center'>
                         <Image width={380} height={205} src="/image/ccc/presenting/middle-school.jpg" alt="Presentation to the Middle School" />
-                        <Link className='m:hidden' href='/combating-climate-change/Combating Climate Change.pptx'>
-                            <Button>
-                                <svg className={Button.Icon} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48' width='48px' height='48px'><path fill='#dc4c2c' d='M8,24c0,9.941,8.059,18,18,18s18-8.059,18-18H26H8z'/><path fill='#f7a278' d='M26,6v18h18C44,14.059,35.941,6,26,6z'/><path fill='#c06346' d='M26,6C16.059,6,8,14.059,8,24h18V6z'/><path fill='#9b341f' d='M22.319,34H5.681C4.753,34,4,33.247,4,32.319V15.681C4,14.753,4.753,14,5.681,14h16.638 C23.247,14,24,14.753,24,15.681v16.638C24,33.247,23.247,34,22.319,34z'/><path fill='#fff' d='M14.673,19.012H10v10h2.024v-3.521H14.3c1.876,0,3.397-1.521,3.397-3.397v-0.058 C17.697,20.366,16.343,19.012,14.673,19.012z M15.57,22.358c0,0.859-0.697,1.556-1.556,1.556h-1.99v-3.325h1.99 c0.859,0,1.556,0.697,1.556,1.556V22.358z'/></svg>
-                                <span>Download Presentation</span>
-                            </Button>
+                        <Link className='button m:hidden' href='/combating-climate-change/Combating Climate Change.pptx'>
+                            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48' width='48px' height='48px'><path fill='#dc4c2c' d='M8,24c0,9.941,8.059,18,18,18s18-8.059,18-18H26H8z'/><path fill='#f7a278' d='M26,6v18h18C44,14.059,35.941,6,26,6z'/><path fill='#c06346' d='M26,6C16.059,6,8,14.059,8,24h18V6z'/><path fill='#9b341f' d='M22.319,34H5.681C4.753,34,4,33.247,4,32.319V15.681C4,14.753,4.753,14,5.681,14h16.638 C23.247,14,24,14.753,24,15.681v16.638C24,33.247,23.247,34,22.319,34z'/><path fill='#fff' d='M14.673,19.012H10v10h2.024v-3.521H14.3c1.876,0,3.397-1.521,3.397-3.397v-0.058 C17.697,20.366,16.343,19.012,14.673,19.012z M15.57,22.358c0,0.859-0.697,1.556-1.556,1.556h-1.99v-3.325h1.99 c0.859,0,1.556,0.697,1.556,1.556V22.358z'/></svg>
+                            <span>Download Presentation</span>
                         </Link>
                     </div>
                     <div className='m:hidden'>
@@ -237,7 +232,7 @@ Permanent link: https://perma.cc/N7QN-QDK6
 }
 
 export function Green({children}: { children: React.ReactNode }) {
-    return <span className='bold text-green-700'>{children}</span>;
+    return <span className='bold text-green-700 dark:text-green-400'>{children}</span>;
 }
 
 export function Savings({ children }: { children: React.ReactNode }) {
