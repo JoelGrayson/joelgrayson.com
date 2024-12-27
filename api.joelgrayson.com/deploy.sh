7720f8f78518f9487c89b4cba8075ca89e61fa04
Joel Grayson <joel@joelgrayson.com>
2024-12-27 12:10:05 -0500

    fix api.joelgrayson.com

--- api.joelgrayson.com/deploy.sh
+++ api.joelgrayson.com/deploy.sh
@@ -5,3 +5,5 @@
 gcloud config set project joelgrayson
 gcloud run deploy --source . api-joelgrayson-com
 
+# https://console.cloud.google.com/run/detail/us-east4/api-joelgrayson-com/revisions?inv=1&invt=AblQgw&project=joelgrayson
+
