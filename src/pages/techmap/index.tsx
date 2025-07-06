e746e345a811782b12fd710c391d7e3da9223c8f
Joel Grayson <joel@joelgrayson.com>
2025-07-06 00:44:19 -0700

    techmap: show map

--- src/pages/techmap/index.tsx
+++ src/pages/techmap/index.tsx
@@ -1,11 +1,22 @@
 import Page from "@/components/page/DefaultPage";
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { useEffect } from "react";
 
 export default function TechMap() {
-    return <Page>
+    return <Page noPadding>
         <h1 className="text-4xl">TechMap</h1>
-
         <p>App link coming soon...</p>
         <p>Web version coming soon...</p>
+        
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_TECHMAP_API_KEY as string}>
+            <Map
+                style={{width: '100vw', height: '100vh'}}
+                defaultCenter={{lat: 22.54992, lng: 0}}
+                defaultZoom={3}
+                gestureHandling={'greedy'}
+                disableDefaultUI={true}
+            />
+        </APIProvider>
     </Page>;
 }
 
