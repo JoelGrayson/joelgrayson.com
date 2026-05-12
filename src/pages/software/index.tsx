b6cc8acb3be9c82f4f03d40eb5269ad28fe55c55
Joel Grayson <joel@joelgrayson.com>
2026-05-12 11:16:40 -0700

    update /software

--- src/pages/software/index.tsx
+++ src/pages/software/index.tsx
@@ -22,11 +22,13 @@ export default function Software() {
             <li>
                 Apps
                 <ul>
-                    <li><Link href='/software/edit-time' target='_blank'>Edit Time</Link> (<Link target='_blank' href='https://apps.apple.com/us/app/edit-time/id6464405876?pt=126612879&ct=web'>App Store Link</Link>)</li>
-                    <li><Link href='/techmap' target='_blank'>TechMap</Link> (<Link target='_blank' href='https://apps.apple.com/us/app/techmap/id6748248815?pt=126612879&ct=web'>App Store Link</Link>)</li>
-                    <li><Link href='https://apps.apple.com/us/app/worderoo/id6745312298?pt=126612879&ct=web' target='_blank'>Worderoo</Link></li>
+                    <li><Link href='https://apps.apple.com/us/app/edit-time/id6464405876?pt=126612879&ct=web' target='_blank'>Edit Time</Link></li>
+                    <li><Link href='https://apps.apple.com/us/app/morsebench/id6767979247?pt=126612879&ct=web' target='_blank'>MorseBench</Link></li>
                     <li><Link href='https://apps.apple.com/us/app/buseroo/id6736895918?pt=126612879&ct=web' target='_blank'>Buseroo App</Link></li>
+                    <li><Link href='https://apps.apple.com/us/app/techmap/id6748248815?pt=126612879&ct=web' target='_blank'>TechMap</Link></li>
                     <li><Link href='https://apps.apple.com/us/app/readometer-track-your-speed/id6757317732?pt=126612879&ct=web' target='_blank'>Readometer</Link></li>
+                    <li><Link href='https://apps.apple.com/us/app/worderoo/id6745312298?pt=126612879&ct=web' target='_blank'>Worderoo</Link></li>
+                    <li><Link href='https://apps.apple.com/us/app/simplehealth-steps-sleep-gym/id6761314044?pt=126612879&ct=web' target='_blank'>SimpleHealth</Link></li>
 
                     {/*
                     Not including/failed:
@@ -41,36 +43,31 @@ export default function Software() {
                 <ul>
                     <li><Link target='_blank' href='/'>joelgrayson.com</Link></li>
                     <li><Link target='_blank' href='https://buseroo.com'>buseroo.com</Link></li>
+                    <li><Link target='_blank' href='https://morsebench.com'>morsebench.com</Link></li>
+                    <li><Link target='_blank' href='https://joelkit.com'>joelkit.com</Link></li>
                     <li><Link target='_blank' href='https://lirongart.com'>lirongart.com</Link></li>
-                    <li><Link target='_blank' href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A'>youtube.com/@JoelGrayson (Slaphappy)</Link></li>
-                    <li><Link target='_blank' href='https://studentsforelectricbuses.org'>studentsforelectricbuses.org</Link></li>
-                    <li><Link target='_blank' href='https://luxpremierllc.com'>luxpremierllc.com</Link></li>
-                    <li><Link target='_blank' href='https://memorizethepresidents.com'>memorizethepresidents.com</Link></li>
+                    <li><Link target='_blank' href='https://www.youtube.com/channel/UCAwfG8BfhLuhMddFZh7z09A'>youtube.com/@JoelGrayson</Link></li>
                     <li><Link target='_blank' href='https://wasian.io'>wasian.io</Link></li>
+                    <li><Link target='_blank' href='https://luxpremierllc.com'>luxpremierllc.com</Link></li>
+                    <li><Link target='_blank' href='https://studentsforelectricbuses.org'>studentsforelectricbuses.org</Link></li>
+                    <li><Link target='_blank' href='https://memorizethepresidents.com'>memorizethepresidents.com</Link></li>
                     <li><Link target='_blank' href='https://shirtocracy.com'>shirtocracy.com</Link></li>
                     <li><Link target='_blank' href='/muffin'>The Muffin Game</Link></li>
-                    <li><Link target='_blank' href='https://joelkit.com'>joelkit.com</Link></li>
                     <li><Link target='_blank' href='https://joelgrayson.wixsite.com/geography'>Geography learning site</Link> (my first site)</li>
                     {/* <li><Link target='_blank' href='https://stanfordlaunches.com'>stanfordlaunches.com</Link> (coming soon)</li> */}
                 </ul>
             </li>
             <li>
-                GIS
-                <ul>
-                    {/* TODO: coming soon: MapRecord and MapNotate */}
-                    <li><Link href='/software/dms-dd-converter' target='_blank'>DMS DD Converter</Link></li>
-                </ul>
-            </li>
-            <li>
                 Developer Tools
                 <ul>
-                    <li><Link href='/software/hide desktop.command' target='_blank'>Hide/Show Desktop Icons</Link></li>
-                    <li><Link href='https://github.com/JoelGrayson/create-joel-app' target='_blank'>Create Joel App</Link></li>
-                    <li><Link href='/software/processing-to-html-converter' target='_blank'>Processing to HTML Converter</Link></li>
-                    <li><Link href='https://github.com/JoelGrayson/jredirects' target='_blank'>JRedirects</Link></li>
                     <li><Link href='/software/drive-download-link-generator' target='_blank'>Drive Download Link Generator</Link></li>
                     <li><Link href='/software/box-download-link-generator' target='_blank'>Box Download Link Generator</Link></li>
                     <li><Link href='/software/dropbox-download-link-generator' target='_blank'>Dropbox Download Link Generator</Link></li>
+                    <li><Link href='/software/dms-dd-converter' target='_blank'>DMS DD Converter</Link></li>
+                    <li><Link href='/software/hide desktop.command' target='_blank'>Hide/Show Desktop Icons</Link></li>
+                    {/* <li><Link href='https://github.com/JoelGrayson/create-joel-app' target='_blank'>Create Joel App</Link></li> */}
+                    <li><Link href='/software/processing-to-html-converter' target='_blank'>Processing to HTML Converter</Link></li>
+                    <li><Link href='https://github.com/JoelGrayson/jredirects' target='_blank'>JRedirects</Link></li>
                     {/*
                     No longer works:
                     <li><Link href='/software/onedrive-download-link-generator' target='_blank'>Onedrive Download Link Generator</Link></li>
@@ -80,11 +77,11 @@ export default function Software() {
             <li>
                 Resources
                 <ul>
-                    <li><Link href='/software/cite/mla' target='_blank'>MLA Citation Generator</Link></li>
-                    <li><Link href='https://www.youtube.com/watch?v=Fi19NDsJB4E' target='_blank'>Zoom Timer</Link></li>
-                    <li><Link href='/software/days-until' target='_blank'>Days Until</Link></li>
                     <li><Link href='/learn/pi' target='_blank'>Memorize π</Link></li>
                     <li><Link href='/learn/number?name=tau' target='_blank'>Memorize τ</Link></li>
+                    <li><Link href='/software/cite/mla' target='_blank'>MLA Citation Generator</Link></li>
+                    <li><Link href='https://www.youtube.com/watch?v=Fi19NDsJB4E' target='_blank'>Zoom Timers</Link></li>
+                    <li><Link href='/software/days-until' target='_blank'>Days Until</Link></li>
                 </ul>
             </li>
         </ul>
